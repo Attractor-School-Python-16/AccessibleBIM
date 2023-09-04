30c822e787dd73be399cf4b8f26d474b9f2961e3
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-04 23:23:16 +0600

    Создал модели AbstractModel, Subscription и UsersSubscription для приложения subscription

---
+++ subscription/models/subscription.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+# Create your models here.
+class Subscription(models.Model):
+    # course = models.ForeignKey('courses.Course', on_delete=models.PROTECT, related_name='courses', verbose_name='Курс')
+    price = models.IntegerField(null=False, blank=False, verbose_name='Цена за курс')
+    discount = models.IntegerField(null=True, blank=True, default=0, verbose_name='Скидка на курс')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Создано',
+                                   blank=True, null=True)
+    updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Обновлено',
+                                   blank=True, null=True)
