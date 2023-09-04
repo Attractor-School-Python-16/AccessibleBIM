a4dcc9b645598d5727ef2928935f258de6b80d5f
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-05 02:38:25 +0600

    #19 Добавил модель CourseTarget

---
+++ modules/models/course_target.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from modules.models.modules import AbstractModel, Module
+
+
+# Create your models here.
+class CourseTarget(AbstractModel):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Для кого предназначен курс')
+    description = models.TextField(max_length=150, null=False, blank=False, verbose_name='Подробное описание')
+
+    class Meta:
+        db_table = 'Course'
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
+
+    def __str__(self):
+        return f'{self.title}'
