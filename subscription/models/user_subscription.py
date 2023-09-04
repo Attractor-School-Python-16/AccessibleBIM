7bba27f0cd5a9210b852875269814b9a126afdef
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-05 00:14:03 +0600

    Поменял связи с M2M на FK в модели UsersSubscription

--- subscription/models/user_subscription.py
+++ subscription/models/user_subscription.py
@@ -6,5 +6,7 @@ from subscription.models.subscription import AbstractModel, Subscription
 
 # Create your models here.
 class UsersSubscription(AbstractModel):
-    subscription = models.ManyToManyField(Subscription, related_name='subscriptions', verbose_name='Подписки')
-    user = models.ManyToManyField(get_user_model(), related_name='users', verbose_name='Пользователи')
+    subscription = models.ForeignKey(Subscription, on_delete=models.DO_NOTHING, related_name='subscriptions',
+                                     verbose_name='Подписки')
+    user = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name='users',
+                             verbose_name='Пользователи')
