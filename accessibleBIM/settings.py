7af9a6bad75a877f46aa6aba22d8993f1eca1a3d
Emir <malcikclashofclans@gmail.com>
2023-09-04 20:57:25 +0600

    Создал приложение test_bim, указал приложения в INSTALLED_APPS

--- accessibleBIM/settings.py
+++ accessibleBIM/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'courses',
+    'step',
+    'progress',
+    'subscription',
+    'test_bim',
 ]
 
 MIDDLEWARE = [
