e9a6d8896144feb29fb2132ecf84f82a41d53099
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-05 00:04:25 +0600

    Добавил класс Meta, добавил комментарии, подправил created_by, updated_by

--- subscription/models/subscription.py
+++ subscription/models/subscription.py
@@ -6,10 +6,11 @@ from django.db import models
 class AbstractModel(models.Model):
     create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
-    # created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Создано',
-    #                                blank=True, null=True)
-    # updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Обновлено',
-    #                                blank=True, null=True)
+
+    # created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='created_by',
+    # verbose_name='Создано', blank=True, null=True)
+    # updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='updated_by',
+    # verbose_name='Обновлено', blank=True, null=True)
 
     class Meta:
         abstract = True
@@ -20,3 +21,12 @@ class Subscription(AbstractModel):
     price = models.IntegerField(null=False, blank=False, verbose_name='Цена за курс')
     discount = models.IntegerField(null=True, blank=True, default=0, verbose_name='Скидка на курс')
     end_date = models.DateTimeField(verbose_name='Дата обновления')
+
+    class Meta:
+        db_table = 'subscription'
+        verbose_name = 'Подписки'
+        verbose_name_plural = 'Подписка'
+
+    # Закоментил до всех миграций так как в данный момент нет нужного поля
+    # def __str__(self):
+    #     return f'{self.course}'
