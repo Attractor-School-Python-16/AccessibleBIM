1982d4dfda2ea8f682d026a92ed746656a97a028
Emir <malcikclashofclans@gmail.com>
2023-09-04 22:36:49 +0600

    Вернул AUTH_USER_MODEL в settings.py

--- accessibleBIM/settings.py
+++ accessibleBIM/settings.py
@@ -117,6 +117,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
