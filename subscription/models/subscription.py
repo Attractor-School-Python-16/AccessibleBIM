f5ceed733dd6a6b0060ae9f0efed0d3da8c0b50b
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-04 23:36:56 +0600

    Добавил поле end_date в модель Subscription

--- subscription/models/subscription.py
+++ subscription/models/subscription.py
@@ -3,13 +3,20 @@ from django.db import models
 
 
 # Create your models here.
-class Subscription(models.Model):
+class AbstractModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    # created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Создано',
+    #                                blank=True, null=True)
+    # updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Обновлено',
+    #                                blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Subscription(AbstractModel):
     # course = models.ForeignKey('courses.Course', on_delete=models.PROTECT, related_name='courses', verbose_name='Курс')
     price = models.IntegerField(null=False, blank=False, verbose_name='Цена за курс')
     discount = models.IntegerField(null=True, blank=True, default=0, verbose_name='Скидка на курс')
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Создано',
-                                   blank=True, null=True)
-    updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='Обновлено',
-                                   blank=True, null=True)
+    end_date = models.DateTimeField(verbose_name='Дата обновления')
