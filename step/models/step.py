ec6e8842e66dc5fd4b3789cc2945e13de42b7f25
Rustem Galiulin <asmoderous@gmail.com>
2023-09-05 14:44:46 +0600

    создал модель video, внес изменения в step - прописал верные модели, раскомментировал связи с создаваемыми

--- step/models/step.py
+++ step/models/step.py
@@ -11,10 +11,10 @@ class StepModel(AbstractModel):
     }
 
     # chapter = models.ForeignKey('courses.ChapterModel', related_name='step', on_delete=models.CASCADE, verbose_name='Chapter')
-    title = models.CharField(max_length= 250, blank=False, null=False)
-    lesson_type = models.CharField(max_length=10, choices=TYPE_CHOICES, blank=False, null=False)
-    # text = models.ForeignKey('step.Text', related_name='step', on_delete=models.RESTRICT, verbose_name='Текст')
-    # video = models.ForeignKey('step.Video', related_name='step', on_delete=models.RESTRICT, verbose_name='Видео')
+    title = models.CharField(max_length= 250, blank=False, null=False, verbose_name='Наименование')
+    lesson_type = models.CharField(max_length=10, choices=TYPE_CHOICES, blank=False, null=False, verbose_name='Тип занятия')
+    text = models.ForeignKey('step.TextModel', related_name='step', on_delete=models.RESTRICT, verbose_name='Текст')
+    video = models.ForeignKey('step.VideoModel', related_name='step', on_delete=models.RESTRICT, verbose_name='Видео')
     # test = models.ForeignKey('step.Test', related_name='step', on_delete=models.RESTRICT, verbose_name='Тест')
-    # file = models.ManyToManyField('step.File', related_name='step', verbose_name='Файл')
+    file = models.ManyToManyField('step.FileModel', related_name='step', verbose_name='Файл')
     learn_time = models.PositiveIntegerField(blank=False, null=False)
