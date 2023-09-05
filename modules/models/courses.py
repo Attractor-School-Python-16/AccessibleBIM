25b817256b631c56542241fdde73140081744b80
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-05 21:52:05 +0600

    #20 Добавил модель Course

---
+++ modules/models/courses.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from modules.models.modules import AbstractModel, Module
+
+
+# Create your models here.
+class Course(AbstractModel):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Название модуля')
+    description = models.TextField(max_length=150, null=False, blank=False, verbose_name='Описание модуля')
+    image = models.ImageField(null=False, blank=False, upload_to='course', verbose_name='Фото для курса')
+    module_id = models.ForeignKey(Module, related_name='courses', on_delete=models.CASCADE)
+    courseTarget_id = models.ForeignKey(Module, related_name='courses', on_delete=models.DO_NOTHING)
+    learnTime = models.IntegerField(null=False, blank=False, default=0, verbose_name='Время на прохождение курса')
+
+    class Meta:
+        db_table = 'Course'
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
+
+    def __str__(self):
+        return f'{self.title}'
