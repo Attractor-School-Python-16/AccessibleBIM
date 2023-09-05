418b9d24a43103c9c6f4a995b5ea27022d198ae9
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-05 21:53:27 +0600

    #21 Добавил модель Chapter

---
+++ modules/models/chapter.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from modules.models.courses import Course
+from modules.models.modules import AbstractModel, Module
+
+
+# Create your models here.
+class Chapter(AbstractModel):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='ct_course',
+                               verbose_name='Курсы')
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Название модуля')
+    description = models.TextField(max_length=150, null=False, blank=False, verbose_name='Описание модуля')
+    serial_number = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'Chapter'
+        verbose_name = 'Главы'
+        verbose_name_plural = 'Глава'
+
+    def save(self, *args, **kwargs):
+        # Это означает, что модель еще не сохранена в базе данных
+        if self._state.adding:
+            # Получаем максимальное значение serial_number из базы данных
+            last_id = Chapter.objects.filter(customer=self.course).aggregate(largest=models.Max('serial_number'))[
+                'largest']
+
+            # Агрегация может вернуть None! Сначала проверьте это.
+            # Если это не none, просто используйте последний указанный идентификатор (который должен быть наибольшим)
+            # и добавьте к нему еще один
+            if last_id is not None:
+                self.serial_number = last_id + 1
+
+        super(Chapter, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.title} {self.description}'
