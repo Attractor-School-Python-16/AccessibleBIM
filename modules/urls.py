3956c7246cb3f0a8573ef7d0c327e47f5041e05f
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-07 00:36:45 +0600

    #2 Начал делать шабон для детального просмотра курса и его программы

--- modules/urls.py
+++ modules/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from modules.views.modules import HomeView
+from modules.views.modules import HomeView, ModulesView, ModulesDetailView
+
+app_name = 'modules'
 
 urlpatterns = [
     path('', HomeView.as_view(), name="index"),
+    path('courses/', ModulesView.as_view(), name="modules"),
+    path('course/1', ModulesDetailView.as_view(), name="module"), # поменять число на PK
 ]
