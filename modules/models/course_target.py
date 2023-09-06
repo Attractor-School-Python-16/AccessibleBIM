bed9a322fb5037c317e928f301fe727f8eb9b99d
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-06 22:54:16 +0600

    #56 Решил баг в приложении modules (Циклические импорты)

--- modules/models/course_target.py
+++ modules/models/course_target.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from modules.models.courses import CourseModel
 from modules.models.modules import AbstractModel
 
 
@@ -7,11 +8,12 @@ from modules.models.modules import AbstractModel
 class CourseTargetModel(AbstractModel):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Для кого предназначен курс')
     description = models.TextField(max_length=150, null=False, blank=False, verbose_name='Подробное описание')
+    courseTarget_id = models.ForeignKey(CourseModel, related_name='courses', on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'Course'
-        verbose_name = 'Модуль'
-        verbose_name_plural = 'Модули'
+        db_table = 'CourseTarget'
+        verbose_name = 'Целевая аудитория'
+        verbose_name_plural = 'Целевая аудитория'
 
     def __str__(self):
         return f'{self.title}'
