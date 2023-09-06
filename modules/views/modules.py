79a3735602535a3747212046e79b31478645c51c
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-07 00:36:45 +0600

    #2 Начал делать шабон для детального просмотра курса и его программы

--- modules/views/modules.py
+++ modules/views/modules.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class ModulesView(TemplateView):
+    template_name = 'modules/modules_view.html'
+
+
+class ModulesDetailView(TemplateView):
+    template_name = 'modules/modules_detail.html'
