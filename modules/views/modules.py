285aaae9845cb9fc7cf9e0a2aff4f1b8f396a343
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-07 21:23:17 +0600

    #2 Добавил шаблоны для шагов

--- modules/views/modules.py
+++ modules/views/modules.py
@@ -11,3 +11,27 @@ class ModulesView(TemplateView):
 
 class ModulesDetailView(TemplateView):
     template_name = 'modules/modules_detail.html'
+
+
+class AccountDetailView(TemplateView):
+    template_name = 'accounts/profile.html'
+
+
+class AccountLoginView(TemplateView):
+    template_name = 'accounts/login.html'
+
+
+class RegisterLoginView(TemplateView):
+    template_name = 'accounts/register.html'
+
+
+class StepVideoView(TemplateView):
+    template_name = 'steps/step_detail_video.html'
+
+
+class StepTextView(TemplateView):
+    template_name = 'steps/step_detail_text.html'
+
+
+class StepFileView(TemplateView):
+    template_name = 'steps/step_detail_file.html'
