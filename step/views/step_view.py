15552dac55eb3c165d73208dbf984fbd7ed29e5b
Rustem Galiulin <asmoderous@gmail.com>
2023-09-07 17:33:33 +0600

    создал шаблон video_detail.html и text_detail.html для включения в шаблоны списка и детального просмотра, предварительно заполнил шаблоны списка и детального просмотра

--- step/views/step_view.py
+++ step/views/step_view.py
@@ -7,24 +7,24 @@ from step.models.step import StepModel
 
 class StepListView(ListView):
     model = StepModel
-    template_name = 'step_list.html'
+    template_name = 'step/step_list.html'
     context_object_name = 'steps'
     paginate_by = 1
 
 
 class StepDetailView(DetailView):
     queryset = StepModel.objects.all()
-    template_name = "step_detail.html"
+    template_name = "step/step_detail.html"
 
 
 class StepUpdateView(UpdateView):
     model = StepModel
     form_class = StepForm
-    template_name = 'step_create.html'
+    template_name = 'step/step_create.html'
     success_url = reverse_lazy('step_list')
 
 
 class StepDeleteView(DeleteView):
     model = StepModel
-    template_name = 'step_delete.html'
+    template_name = 'step/step_delete.html'
     success_url = reverse_lazy('step_list')
