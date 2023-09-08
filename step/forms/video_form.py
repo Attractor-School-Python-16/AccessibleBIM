514b46f4ea53dbcc5042e06210fbdab4a4847694
Rustem Galiulin <asmoderous@gmail.com>
2023-09-08 12:48:24 +0600

    создал форму для текста, внес изменения в форму видео

--- step/forms/video_form.py
+++ step/forms/video_form.py
@@ -6,14 +6,14 @@ from step.models import VideoModel
 class VideoForm(forms.ModelForm):
     class Meta:
         model = VideoModel
-        fields = ['title', 'description', 'video_file']
+        fields = '__all__'
         labels = {
-            'title': 'Введите наименование видео',
-            'description': 'Введите описание видео',
+            'video_title': 'Введите наименование видео',
+            'video_description': 'Введите описание видео',
             'video_file': 'Загрузите видео-файл',
         }
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'video_title': forms.TextInput(attrs={'class': 'form-control'}),
+            'video_description': forms.Textarea(attrs={'class': 'form-control'}),
             'video_file': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
