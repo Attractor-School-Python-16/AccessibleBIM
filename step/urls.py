e27b507eacd97690560ca88c206d17f7d61bd9ee
Rustem Galiulin <asmoderous@gmail.com>
2023-09-08 12:43:45 +0600

    прописал пути, наполнил шаблоны

--- step/urls.py
+++ step/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from step.views.step_view import StepListView, StepDetailView, StepUpdateView, StepDeleteView, StepCreateView
+from step.views.video_view import VideoListView, VideoDetailView, VideoCreateView, VideoUpdateView, VideoDeleteView
 
 app_name = 'step'
 
@@ -10,4 +11,9 @@ urlpatterns = [
     path('step/create/', StepCreateView.as_view(), name='step_create'),
     path('step/<int:pk>/update/', StepUpdateView.as_view(), name='step_update'),
     path('step/<int:pk>/delete/', StepDeleteView.as_view(), name='step_delete'),
+    path('videos/', VideoListView.as_view(), name='video_list'),
+    path('video/<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
+    path('video/create/', VideoCreateView.as_view(), name='video_create'),
+    path('video/<int:pk>/update/', VideoUpdateView.as_view(), name='video_update'),
+    path('video/<int:pk>/delete/', VideoDeleteView.as_view(), name='video_delete'),
 ]
