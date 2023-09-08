87a0e140f836d4e635c6a7305c9406bd334774a7
Rustem Galiulin <asmoderous@gmail.com>
2023-09-08 12:25:45 +0600

    прописал CRUD для видео, создал шаблоны, внес изменения в модели по наименованиям, в том числе в модель теста. Переписал логику по созданию Step.

--- step/models/text.py
+++ step/models/text.py
@@ -4,6 +4,14 @@ from modules.models import AbstractModel
 
 
 class TextModel(AbstractModel):
-    title = models.CharField(max_length=250, blank=False, null=False, verbose_name="Наименование")
-    description = models.CharField(max_length=500, verbose_name="Описание")
+    text_title = models.CharField(max_length=250, blank=False, null=False, verbose_name="Наименование")
+    text_description = models.CharField(max_length=500, verbose_name="Описание")
     content = models.TextField(max_length=100000, blank=False, null=False, verbose_name="Содержимое")
+
+    def __str__(self):
+        return f'Текст {self.id} {self.text_title}'
+
+    class Meta:
+        db_table = 'texts'
+        verbose_name = 'Текст'
+        verbose_name_plural = 'Тексты'
