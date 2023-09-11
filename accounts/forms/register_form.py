9bcf89f45460338eeb65ccfdc8d3c196d51533ba
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-11 23:08:06 +0600

    Изменила авторизацию на вход по email

--- accounts/forms/register_form.py
+++ accounts/forms/register_form.py
@@ -1,13 +1,14 @@
 from django import forms
 from captcha.fields import CaptchaField
+from django.contrib.auth.forms import UserCreationForm
 
 from accounts.models import CustomUser
 
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     captcha = CaptchaField()
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'father_name', 'email', 'password',
-                  'phone_number', 'job_title', 'type_corp', 'company']  # нужно добавить 'country'
+        fields = ['first_name', 'last_name', 'father_name', 'email', 'password1',
+                  'password2', 'phone_number', 'job_title', 'type_corp', 'company']  # нужно добавить 'country'
