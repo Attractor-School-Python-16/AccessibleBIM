480a7595e69b912d0a65ccdf7a2d1e91d3212da2
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-11 22:36:29 +0600

    Настроила показ кнопок для аутентификации/регистрации и добавила капчу

--- accounts/forms/register_form.py
+++ accounts/forms/register_form.py
@@ -1,10 +1,13 @@
 from django import forms
+from captcha.fields import CaptchaField
 
 from accounts.models import CustomUser
 
 
 class RegisterForm(forms.ModelForm):
+    captcha = CaptchaField()
+
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'father_name', 'email', 'password',
-                  'phone_number', 'job_title', 'company']  # нужно добавить 'country'
+                  'phone_number', 'job_title', 'type_corp', 'company']  # нужно добавить 'country'
