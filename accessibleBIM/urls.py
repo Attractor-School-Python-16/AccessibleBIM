e8663c04ff222b9d4bacf925041fab16b860c655
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-09-12 22:26:54 +0600

    Переименовала файл с представлением на progress_test_answers_view.py (добавила answers), т.к. перепутала название модели. Изменила создание объекта с представления на функцию, т.к. создание должно быть авто-ким.

--- accessibleBIM/urls.py
+++ accessibleBIM/urls.py
@@ -22,4 +22,5 @@ from accessibleBIM import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('modules.urls')),
+    path('', include('progress.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
