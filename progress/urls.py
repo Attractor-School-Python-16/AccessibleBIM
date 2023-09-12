c5b470cb2522ccfd9ba2381f527340a18781f3e7
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-09-12 22:26:54 +0600

    Переименовала файл с представлением на progress_test_answers_view.py (добавила answers), т.к. перепутала название модели. Изменила создание объекта с представления на функцию, т.к. создание должно быть авто-ким.

--- progress/urls.py
+++ progress/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from progress.views.progress_test_answers_view import ProgressTestAnswersCreateView, ProgressTestAnswersDeleteView
+
+app_name = 'progress'
+
 urlpatterns = [
+    # path('create/', ProgressTestAnswersCreateView.as_view(), name='create_progress_test'),
+    path('<int:pk>/delete/', ProgressTestAnswersDeleteView.as_view(), name='delete_progress_test'),
 
 ]
---
+++ progress/views/progress_test_answers_view.py
@@ -0,0 +1,32 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView
+
+from progress.models import ProgressTestAnswers
+
+
+def create_progress_tests_answers(progress_test, question, answer):
+    # Функция создает и сохраняет объект ProgressTestAnswers
+    progress_test_answer = ProgressTestAnswers.objects.create(
+        progress_test=progress_test,
+        question=question,
+        answer=answer
+    )
+    progress_test_answer.save()
+
+
+# закомментировала создание ProgressTestAnswers, т.к. нам нужно,
+# чтобы в БД эти данные появлялись авто-ки после начала теста. Но возможно для статистики это понадобится,
+# поэтому пока не удаляю.
+# class ProgressTestAnswersCreateView(CreateView):
+#     model = ProgressTestAnswers
+#     fields = ['user', 'progress_test', 'question', 'answer']
+#     template_name = "progress_test_answers_create.html"
+
+
+class ProgressTestAnswersDeleteView(DeleteView):
+    model = ProgressTestAnswers
+    success_url = reverse_lazy('index.html')       #как будет главная админки, нужно поменять путь
+    template_name = 'progress_test_answers_delete.html'
+
+
+
