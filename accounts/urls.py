df280860b5ecc67c702c5617538f2a5b3f08f22b
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-12 14:02:19 +0600

    #73 Добавила профиль

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, ProfileView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="accounts/login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
