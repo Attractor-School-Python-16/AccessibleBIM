e115dca277752d13056dc70e3328f6e5db3427a9
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-13 01:46:57 +0600

    Добавила отпрвку писем с верификацией email адресов

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,9 @@ from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,
 from django.shortcuts import redirect
 from django.urls import path
 
-from accounts.views import RegisterView, ProfileView
+from accounts.views import RegisterView, ProfileView, VerificationEmailSentView, VerificationEmailNotSentView, \
+    InvalidVerificationLinkView
+from accounts.views.register_view import activate
 
 app_name = 'accounts'
 
@@ -10,6 +12,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="accounts/login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('email-verification/', VerificationEmailSentView.as_view(), name="verification_sent"),
+    path('email-verification/sending-error/', VerificationEmailNotSentView.as_view(), name="verification_sending_error"),
+    path('email-verification/invalid-link/', InvalidVerificationLinkView.as_view(), name="invalid_verification_link"),
+    path('email-verification/<uidb64>/<token>', activate, name='activate'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('password-change/', PasswordChangeView.as_view(template_name="accounts/change_password.html",
                                                         success_url='/password-change/done/'), name="change_password"),
