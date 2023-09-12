770cfaa5ec0aed02112b3c29781a8629a9177b06
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-09-13 00:26:01 +0600

    Создала функцию для создания объекта ProgressTest, представление для его удаления, шаблоны для них. По логике, изменение и детальный просмотр для этой модели не нужны.

--- progress/views/progress_test_answers_view.py
+++ progress/views/progress_test_answers_view.py
@@ -4,6 +4,8 @@ from django.views.generic import CreateView, DeleteView
 from progress.models import ProgressTestAnswers
 
 
+#функцию нужно вызвать во вьюшке, которая создает возможность прохождения теста для юзера, чтобы функция
+#вызывалась после клика на кнопку Далее после ответа на первый вопрос
 def create_progress_tests_answers(progress_test, question, answer):
     # Функция создает и сохраняет объект ProgressTestAnswers
     progress_test_answer = ProgressTestAnswers.objects.create(
@@ -19,14 +21,14 @@ def create_progress_tests_answers(progress_test, question, answer):
 # поэтому пока не удаляю.
 # class ProgressTestAnswersCreateView(CreateView):
 #     model = ProgressTestAnswers
-#     fields = ['user', 'progress_test', 'question', 'answer']
+#     fields = ['progress_test', 'question', 'answer']
 #     template_name = "progress_test_answers_create.html"
 
 
 class ProgressTestAnswersDeleteView(DeleteView):
     model = ProgressTestAnswers
     success_url = reverse_lazy('index.html')       #как будет главная админки, нужно поменять путь
-    template_name = 'progress_test_answers_delete.html'
+    template_name = 'progress_test_answers_delete.html'    #использовала шаблон от удаления Step
 
 
 
