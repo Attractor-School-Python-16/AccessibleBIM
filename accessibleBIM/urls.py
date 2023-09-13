fe6600e6912aea56198976af55a038394c6c15f5
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-13 20:59:23 +0600

    #29 Создал CRUD для Subscription + реализовал шаблоны для него

--- accessibleBIM/urls.py
+++ accessibleBIM/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('modules.urls')),
     path('', include('accounts.urls')),
+    path('', include('subscription.urls')),
     path('captcha/', include('captcha.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
