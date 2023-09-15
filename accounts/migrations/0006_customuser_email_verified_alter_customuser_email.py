6dd433786bb6db91fbd12446260d40e67c7ef8c1
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-15 14:13:48 +0600

    Исправила миграции

--- accounts/migrations/0006_customuser_email_verified_alter_customuser_email.py
+++ accounts/migrations/0006_customuser_email_verified_alter_customuser_email.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0004_alter_customuser_email'),
+        ('accounts', '0005_rename_subscription_customuser_subscriptions'),
     ]
 
     operations = [
