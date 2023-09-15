2061878cccbc400951f27d3a8298432c67d9fe97
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-15 15:13:32 +0600

    Доработала тест TestCustomUser

--- accounts/tests/tests_views/test_customuser.py
+++ accounts/tests/tests_views/test_customuser.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 from django.test import TestCase
+from django.urls import reverse
 
+from accounts.forms import RegisterForm
 from accounts.models import CustomUser
 
 
@@ -10,13 +12,24 @@ class TestCustomUser(TestCase):
         self.correct_data = {
             'first_name': 'my_name',
             'last_name': 'my_surname',
+            'father_name': '',
             'email': 'my_user@bimtest.com',
+            'password1': '123',
+            'password2': '123',
             'phone_number': '+996555555555',
+            'job_title': '',
+            'type_corp': '7',
+            'company': '',
+            'captcha_0': 'dummy-value',
+            'captcha_1': 'PASSED',
         }
 
     def test_register(self):
+        form = RegisterForm(self.correct_data)
+        self.assertTrue(form.is_valid())
         response = self.client.post("/register/", data=self.correct_data)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('accounts:verification_sent'))
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(CustomUser.objects.first().first_name, self.correct_data['first_name'])
         self.assertEqual(CustomUser.objects.first().last_name, self.correct_data['last_name'])
