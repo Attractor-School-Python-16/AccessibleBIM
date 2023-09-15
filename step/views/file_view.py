84f0724ae65086f6e33dc374aaef9b0c01a78292
Rustem Galiulin <asmoderous@gmail.com>
2023-09-15 18:36:22 +0600

    вернул m2m связь файлов со степами по требованию заказчика, прописал логику по созданию, редакутированию степа, доработал crud файла и видео

--- step/views/file_view.py
+++ step/views/file_view.py
@@ -7,28 +7,29 @@ from step.models import FileModel
 
 class FileListView(ListView):
     model = FileModel
-    template_name = 'file/file_list.html'
+    template_name = 'steps/file/file_list.html'
     context_object_name = 'files'
 
 
 class FileDetailView(DetailView):
     queryset = FileModel.objects.all()
-    template_name = "file/file_detail.html"
+    template_name = "steps/file/file_detail.html"
+    context_object_name = 'file'
 
 
 class FileCreateView(CreateView):
     form_class = FileForm
-    template_name = "file/file_create.html"
+    template_name = "steps/file/file_create.html"
 
 
 class FileUpdateView(UpdateView):
     model = FileModel
     form_class = FileForm
-    template_name = 'file/file_update.html'
+    template_name = 'steps/file/file_update.html'
     success_url = reverse_lazy('file_list')
 
 
 class FileDeleteView(DeleteView):
     model = FileModel
-    template_name = 'file/file_delete.html'
+    template_name = 'steps/file/file_delete.html'
     success_url = reverse_lazy('file_list')
