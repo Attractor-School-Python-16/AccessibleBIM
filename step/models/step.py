587de8c175e4680bdf78da5352e852e2a0a3a9d9
Rustem Galiulin <asmoderous@gmail.com>
2023-09-16 01:49:40 +0600

    устранил недочеты после ревью, смерждил миграции

--- step/models/step.py
+++ step/models/step.py
@@ -11,14 +11,14 @@ class StepModel(AbstractModel):
         ('test', 'Тест')
     }
 
-    chapter = models.ForeignKey('modules.ChapterModel', related_name='step', on_delete=models.CASCADE, verbose_name='Chapter', blank=True, null=True)
+    chapter = models.ForeignKey('modules.ChapterModel', related_name='step', on_delete=models.CASCADE, verbose_name='Глава', blank=True, null=True)
     title = models.CharField(max_length=250, blank=False, null=False, verbose_name='Наименование')
     lesson_type = models.CharField(max_length=10, choices=TYPE_CHOICES, blank=False, null=False,
                                    verbose_name='Тип занятия')
     text = models.ForeignKey('step.TextModel', related_name='step', on_delete=models.RESTRICT, verbose_name='Текст', blank=True, null=True)
     video = models.ForeignKey('step.VideoModel', related_name='step', on_delete=models.RESTRICT, verbose_name='Видео', blank=True, null=True)
     test = models.ForeignKey('test_bim.TestBim', related_name='step', on_delete=models.RESTRICT, verbose_name='Тест', blank=True, null=True)
-    file = models.ManyToManyField('step.FileModel', related_name='step', verbose_name='Файлы', blank=True, null=True)
+    file = models.ManyToManyField('step.FileModel', related_name='step', verbose_name='Файлы')
     learn_time = models.PositiveIntegerField(blank=False, null=False)
 
     def get_absolute_url(self):
