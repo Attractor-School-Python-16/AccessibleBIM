7b322cc4306c9a5693c22b9aa8ea1ee9f9f3a996
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-16 18:03:55 +0600

    Доработала тест TestCustomUser - test_change_password

--- accounts/tests/factories/user.py
+++ accounts/tests/factories/user.py
@@ -7,8 +7,16 @@ from accounts.models import CustomUser
 class UserFactory(DjangoModelFactory):
     first_name = factory.Sequence(lambda t: f"User{t}")
     last_name = factory.Sequence(lambda t: f"User{t}")
-    email = factory.Sequence(lambda t: f"user{t}@bimtest.com")
+    father_name = ""
+    password1 = '123'
+    password2 = '123'
     phone_number = '+996555555555'
+    job_title = ''
+    company = ''
+    type_corp = '7'
+    email = factory.Sequence(lambda t: f"user{t}@bimtest.com")
+    captcha_0 = 'dummy-value'
+    captcha_1 = 'PASSED'
 
     class Meta:
         model = CustomUser
