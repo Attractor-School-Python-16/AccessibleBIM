084aa3eeaf781b277a45140047bc72ce789cd1bd
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-18 21:33:48 +0600

    #90 Допилил функционал админской части для моделей и шаблонов для выдачи и отключения подписок

--- subscription/forms/subscription_user_add_form.py
+++ subscription/forms/subscription_user_add_form.py
@@ -1,16 +1,5 @@
 from django import forms
 
-from subscription.models import SubscriptionModel
 
-
-class SubscriptionUserAddForm(forms.ModelForm):
-    class Meta:
-        model = SubscriptionModel
-        fields = ['course']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
+class SubscriptionUserAddForm(forms.Form):
+    button = forms.CharField(max_length=100, required=False, label="Выдать подписку")
