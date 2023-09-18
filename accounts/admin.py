d2ce93169984183c48addcf9a15c20f9ac6c6e2b
Rustem Galiulin <asmoderous@gmail.com>
2023-09-18 20:04:21 +0600

    добавил в admin.py приложения accounts связь юзера с группами и доступами. Но теперь в админке юзеры отображаются несколько по-другому - отсутствуют определенные поля. Для админки не должно быть критично, созданы админ и модератор

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from accounts.models import CustomUser
 
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ('groups', 'user_permissions')
+
+admin.site.register(CustomUser, CustomUserAdmin)
