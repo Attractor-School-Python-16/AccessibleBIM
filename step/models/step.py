d51c8e93178fdc3cb18b58b9e6e329ba0c92c9e9
Emir <malcikclashofclans@gmail.com>
2023-09-20 22:37:27 +0600

    #175 Добавил класс TypeChoices

--- step/models/step.py
+++ step/models/step.py
@@ -1,23 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from modules.models import AbstractModel
 
 
 class StepModel(AbstractModel):
-    VIDEO = 'video'
-    TEXT = 'text'
-    TEST = 'test'
-    TYPE_CHOICES = {
-        (VIDEO, 'Видео'),
-        (TEXT, 'Текст'),
-        (TEST, 'Тест')
-    }
+    class TypeChoices(models.TextChoices):
+        VIDEO = 'video', _('Видео')
+        TEXT = 'text', _('Текст')
+        TEST = 'test', _('Тест')
 
     chapter = models.ForeignKey('modules.ChapterModel', related_name='step', on_delete=models.CASCADE, verbose_name='Глава', blank=True, null=True)
     title = models.CharField(max_length=250, blank=False, null=False, verbose_name='Наименование')
-    lesson_type = models.CharField(max_length=10, choices=TYPE_CHOICES, blank=False, null=False,
-                                   verbose_name='Тип занятия')
+    lesson_type = models.CharField(max_length=10, choices=TypeChoices.choices, blank=False, null=False, verbose_name='Тип занятия')
     text = models.ForeignKey('step.TextModel', related_name='step', on_delete=models.RESTRICT, verbose_name='Текст', blank=True, null=True)
     video = models.ForeignKey('step.VideoModel', related_name='step', on_delete=models.RESTRICT, verbose_name='Видео', blank=True, null=True)
     test = models.ForeignKey('quiz_bim.QuizBim', related_name='step', on_delete=models.RESTRICT, verbose_name='Тест', blank=True, null=True)
