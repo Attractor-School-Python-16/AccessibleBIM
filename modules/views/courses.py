09f2062693a5c276dd7049608f83cdab5a6db5cc
Raim <raimdzhan.latypov@gmail.com>
2023-09-21 03:20:43 +0600

    #144 #145 #146 Добавил permissions в представления. Добавил класс PermissionRequiredMixin и соотвествующее значение permission_required в представления modules, courses, chapters, teachers, course_target.

--- modules/views/courses.py
+++ modules/views/courses.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 
@@ -5,40 +6,45 @@ from modules.forms.courses_form import CoursesForm
 from modules.models import CourseModel
 
 
-class CoursesListView(ListView):
+class CoursesListView(PermissionRequiredMixin, ListView):
     model = CourseModel
     template_name = 'courses/courses_list.html'
     context_object_name = 'courses'
     ordering = ("-create_at",)
+    permission_required = 'modules.view_coursemodel'
 
 
-class CourseCreateView(CreateView):
+class CourseCreateView(PermissionRequiredMixin, CreateView):
     template_name = "courses/course_create.html"
     model = CourseModel
     form_class = CoursesForm
+    permission_required = 'modules.add_coursemodel'
 
     def get_success_url(self):
         return reverse("modules:course_detail", kwargs={"pk": self.object.pk})
 
 
-class CourseDetailView(DetailView):
+class CourseDetailView(PermissionRequiredMixin, DetailView):
     model = CourseModel
     context_object_name = 'course'
     template_name = 'courses/course_detail.html'
+    permission_required = 'modules.view_coursemodel'
 
 
-class CourseUpdateView(UpdateView):
+class CourseUpdateView(PermissionRequiredMixin, UpdateView):
     model = CourseModel
     form_class = CoursesForm
     context_object_name = 'course'
     template_name = 'courses/course_update.html'
+    permission_required = 'modules.change_coursemodel'
 
     def get_success_url(self):
         return reverse("modules:course_detail", kwargs={"pk": self.object.pk})
 
 
-class CourseDeleteView(DeleteView):
+class CourseDeleteView(PermissionRequiredMixin, DeleteView):
     model = CourseModel
     template_name = "courses/course_delete.html"
     context_object_name = 'course'
     success_url = reverse_lazy("modules:courses_list")
+    permission_required = 'modules.delete_coursemodel'
