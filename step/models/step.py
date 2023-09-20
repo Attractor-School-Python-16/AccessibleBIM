4a1abe2b8d0f42bccfa7db9925b26b01a8c4f89e
RustemGaliulin <asmoderous@gmail.com>
2023-09-21 02:26:23 +0600

    #149 создание занятия реализовал создание занятия с привязкой к главе, реализовал привязку теста к занятию, создание теста при этом будет выполняться отдельно из-за сложной структуры

--- step/models/step.py
+++ step/models/step.py
@@ -23,7 +23,7 @@ class StepModel(AbstractModel):
     serial_number = models.PositiveIntegerField(blank=False, null=False)
 
     def get_absolute_url(self):
-        return reverse("step:step_list")
+        return reverse("step:step_detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return f'Занятие {self.id} {self.title}'
