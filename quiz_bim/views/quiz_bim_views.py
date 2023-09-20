ec12f30abcab723ccf88d68ed70db509982a1b0c
Raim <raimdzhan.latypov@gmail.com>
2023-09-21 02:32:59 +0600

    #144 #145 #146 Добавил permissions в представления. Добавил класс PermissionRequiredMixin и соотвествующее значение permission_required.

--- quiz_bim/views/quiz_bim_views.py
+++ quiz_bim/views/quiz_bim_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
@@ -5,16 +6,18 @@ from quiz_bim.models.quiz_bim import QuizBim
 from quiz_bim.forms.quiz_bim_form import QuizBimForm
 
 
-class QuizBimListView(ListView):
+class QuizBimListView(PermissionRequiredMixin, ListView):
     model = QuizBim
     template_name = 'quiz_bim/quiz_bim/quiz_bim_list.html'
     context_object_name = 'tests'
+    permission_required = 'quiz_bim.view_testbim'
 
 
-class QuizBimDetailView(DetailView):
+class QuizBimDetailView(PermissionRequiredMixin, DetailView):
     queryset = QuizBim.objects.all()
     template_name = "quiz_bim/quiz_bim/quiz_bim_detail.html"
     context_object_name = 'test'
+    permission_required = 'quiz_bim.view_testbim'
 
     def get_context_data(self, **kwargs):
         questions = self.object.question_bim.all()
@@ -22,26 +25,29 @@ class QuizBimDetailView(DetailView):
         return super().get_context_data(**kwargs)
 
 
-class QuizBimCreateView(CreateView):
+class QuizBimCreateView(PermissionRequiredMixin, CreateView):
     form_class = QuizBimForm
     template_name = "quiz_bim/quiz_bim/quiz_bim_create.html"
+    permission_required = 'quiz_bim.add_testbim'
 
     def get_success_url(self):
         return reverse("quiz_bim:test_detail", kwargs={"pk": self.object.pk})
 
 
-class QuizBimUpdateView(UpdateView):
+class QuizBimUpdateView(PermissionRequiredMixin, UpdateView):
     model = QuizBim
     form_class = QuizBimForm
     template_name = 'quiz_bim/quiz_bim/quiz_bim_update.html'
     success_url = reverse_lazy('quiz_bim:tests_list')
     context_object_name = 'test'
+    permission_required = 'quiz_bim.change_testbim'
 
 
-class QuizBimDeleteView(DeleteView):
+class QuizBimDeleteView(PermissionRequiredMixin, DeleteView):
     model = QuizBim
     context_object_name = 'test'
     template_name = 'quiz_bim/quiz_bim/quiz_bim_delete.html'
     success_url = reverse_lazy('quiz_bim:tests_list')
+    permission_required = 'quiz_bim.delete_testbim'
 
 
