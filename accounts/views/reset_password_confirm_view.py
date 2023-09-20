55072df0c5c02df2f049f1cb429bfe0fe6192810
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-21 00:16:58 +0600

    #180 user permissions Заменила некоторые джанговские вьюшки на кастомные, чтобы прописать права. Добавила пермишены для отображения профиля и смены проля.

---
+++ accounts/views/reset_password_confirm_view.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.views import PasswordResetConfirmView
+
+
+class ResetPasswordConfirmView(PasswordResetConfirmView):
+    template_name = 'accounts/password-reset/password_reset_confirm.html',
+    success_url = '/password-reset-complete/'
