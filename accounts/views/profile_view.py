317d7741ecaf5a593588086551f369a87df2ff82
RustemGaliulin <asmoderous@gmail.com>
2023-09-20 23:56:41 +0600

    #149 добавление страницы модератора Добавил страницу модератора и представление в modules.py. Изменил тип представления в профиле юзера для проверки принадлежности к группе.

--- accounts/views/profile_view.py
+++ accounts/views/profile_view.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 from django.views import View
+from django.views.generic import DetailView
+
+from accounts.models import CustomUser
 
 
-class ProfileView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, template_name='accounts/profile.html')
-        #необходимо добавить курсы из подписок в контекст
+class ProfileView(DetailView):
+    model = CustomUser
+    template_name = 'accounts/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        is_moderator = self.request.user.groups.filter(name='moderators').exists()
+        context['is_moderator'] = is_moderator
+        return context
