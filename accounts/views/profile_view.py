3e03b451bc4d019d0ebcb54f5097c841a40c3ff3
Emir <malcikclashofclans@gmail.com>
2023-09-21 22:41:22 +0600

    #186 Исправил отображение в профиле. Убрал дупликат "Мои курсы", поправил отображение карточек курсов в шаблоне профиля

--- accounts/views/profile_view.py
+++ accounts/views/profile_view.py
@@ -9,5 +9,7 @@ from accounts.models import CustomUser
 class ProfileView(View):
     def get(self, request, *args, **kwargs):
         is_moderator = self.request.user.groups.filter(name='moderators').exists()
-        context = {'is_moderator': is_moderator}
+        context = {
+            'is_moderator': is_moderator
+        }
         return render(request, 'accounts/profile.html', context)
