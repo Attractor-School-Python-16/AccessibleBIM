942e3981040e1337c3f95a3bae52f742950fa533
RustemGaliulin <asmoderous@gmail.com>
2023-09-21 16:23:40 +0600

    #149 профиль Вернул представление профиля через View, убрал pk из запроса

--- accounts/views/profile_view.py
+++ accounts/views/profile_view.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 
 from django.views import View
-from django.views.generic import DetailView
 
 from accounts.models import CustomUser
 
 
-class ProfileView(DetailView):
-    model = CustomUser
-    template_name = 'accounts/profile.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+class ProfileView(View):
+    def get(self, request, *args, **kwargs):
         is_moderator = self.request.user.groups.filter(name='moderators').exists()
-        context['is_moderator'] = is_moderator
-        return context
+        context = {'is_moderator': is_moderator}
+        return render(request, 'accounts/profile.html', context)
