ee10184357df53826ac5406ba28c2d084f24d9ba
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-21 14:08:02 +0600

    #180 user permissions Разделила вьюшки по папкам, прописала импорты

--- accounts/views/reset_password/reset_password_confirm_view.py
+++ accounts/views/reset_password/reset_password_confirm_view.py
@@ -2,5 +2,5 @@ from django.contrib.auth.views import PasswordResetConfirmView
 
 
 class ResetPasswordConfirmView(PasswordResetConfirmView):
-    template_name = 'accounts/password-reset/password_reset_confirm.html',
+    template_name = 'accounts/password-reset/password_reset_confirm.html'
     success_url = '/password-reset-complete/'
