2ab31112349c1874c1b92ee358fbf41eb8fa3fce
Emir <malcikclashofclans@gmail.com>
2023-09-23 23:23:19 +0600

    #200 Стилизовал логинку Установил библиотеку django-crispy-forms Убрал лишний код для стилизации виджетов

--- modules/forms/chapters_form.py
+++ modules/forms/chapters_form.py
@@ -7,10 +7,3 @@ class ChaptersForm(forms.ModelForm):
     class Meta:
         model = ChapterModel
         fields = ['title', 'description', 'serial_number']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
