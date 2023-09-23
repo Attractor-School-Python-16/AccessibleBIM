a80d5c232b56c336ed186a5a20dc594513e5a817
Emir <malcikclashofclans@gmail.com>
2023-09-23 23:23:19 +0600

    #200 Стилизовал логинку Установил библиотеку django-crispy-forms Убрал лишний код для стилизации виджетов

--- subscription/forms/subscription_form.py
+++ subscription/forms/subscription_form.py
@@ -7,10 +7,3 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = SubscriptionModel
         fields = ['course', 'price', 'discount']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
