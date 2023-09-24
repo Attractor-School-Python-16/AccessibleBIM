4b03f7a57085b5272868e008935f0cc3bac399df
Emir <malcikclashofclans@gmail.com>
2023-09-24 15:17:15 +0600

    #112, #113, #114 Базовые тесты для UpdateView Написал базовые тесты для QuizBimUpdateView, QuestionBimUpdateView, AnswerBimUpdateView

--- quiz_bim/tests/view_tests/answer_tests.py
+++ quiz_bim/tests/view_tests/answer_tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from quiz_bim.models import AnswerBim
-from quiz_bim.tests.factories import QuestionBimFactory
+from quiz_bim.tests.factories import QuestionBimFactory, AnswerBimFactory
 
 
 class TestAnswerBimCreateView(TestCase):
@@ -33,3 +33,34 @@ class TestAnswerBimCreateView(TestCase):
         response = self.client.post(self.url, data=invalid_data)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(previous_count, AnswerBim.objects.count())
+
+
+class TestAnswerBimUpdateView(TestCase):
+    answer = None
+
+    def setUp(self) -> None:
+        self.answer = AnswerBimFactory.create()
+        self.url = reverse("quiz_bim:answer_update", kwargs={"pk": self.answer.pk})
+
+    def test_update_view(self):
+        new_data = {
+            "answer": "New title",
+            "is_correct": False
+        }
+        response = self.client.post(self.url, data=new_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.answer.refresh_from_db()
+        self.assertEqual(self.answer.answer, "New title")
+        self.assertEqual(self.answer.is_correct, False)
+
+    def test_invalid_data(self):
+        invalid_data = {
+            "title": "",
+            "is_correct": "string"
+        }
+        response = self.client.post(self.url, data=invalid_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_not_found(self):
+        response = self.client.get(reverse("quiz_bim:answer_update", kwargs={"pk": 999}))
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
