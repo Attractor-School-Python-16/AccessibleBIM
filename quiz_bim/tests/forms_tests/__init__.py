0c8934284acb6035df14ed0c2cf9afe6126a1d87
Emir <malcikclashofclans@gmail.com>
2023-09-24 13:40:35 +0600

    #112, #113, #114 Тесты для форм

---
+++ quiz_bim/tests/forms_tests/__init__.py
@@ -0,0 +1,3 @@
+from .answer_tests import *
+from .question_tests import *
+from .quiz_tests import *
---
+++ quiz_bim/tests/forms_tests/answer_tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from quiz_bim.forms.answer_bim_form import AnswerBimForm
+from quiz_bim.tests.factories import QuestionBimFactory
+
+
+class TestQuestionBimForm(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.correct_form_data = {
+            "answer": "Answer",
+            "is_correct": True,
+            "question_bim": QuestionBimFactory.create()
+        }
+        super().setUpClass()
+
+    def test_answer_form(self):
+        form = AnswerBimForm(data=self.correct_form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_answer_form_invalid(self):
+        invalid_data = {
+            "answer": "",
+            "is_correct": None,
+            "question_bim": "string"
+        }
+        form = AnswerBimForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
