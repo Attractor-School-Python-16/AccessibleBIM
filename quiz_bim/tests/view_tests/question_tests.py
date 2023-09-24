6e796f9ee1af1baf733e425167ef7feb100f1036
Emir <malcikclashofclans@gmail.com>
2023-09-24 14:51:14 +0600

    #112, #113, #114 Базовые тесты для CreateView Написал базовые тесты для QuizBimCreateView, QuestionBimCreateView, AnswerBimCreateView

--- quiz_bim/tests/view_tests/question_tests.py
+++ quiz_bim/tests/view_tests/question_tests.py
@@ -3,7 +3,8 @@ from http import HTTPStatus
 from django.test import *
 from django.urls import reverse
 
-from quiz_bim.tests.factories import QuestionBimFactory
+from quiz_bim.models import QuestionBim
+from quiz_bim.tests.factories import QuestionBimFactory, QuizBimFactory
 
 
 class TestQuestionBimDetailView(TestCase):
@@ -19,3 +20,29 @@ class TestQuestionBimDetailView(TestCase):
     def test_not_found(self):
         response = self.client.get(reverse("quiz_bim:question_detail", kwargs={"pk": 999}))
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+
+class TestQuestionBimCreateView(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.correct_form_data = {
+            "title": "Question"
+        }
+        quiz = QuizBimFactory.create()
+        cls.url = reverse("quiz_bim:question_create", kwargs={"pk": quiz.pk})
+        super().setUpClass()
+
+    def test_create_view(self):
+        previous_count = QuestionBim.objects.count()
+        response = self.client.post(self.url, data=self.correct_form_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(QuestionBim.objects.count() - previous_count, 1)
+
+    def test_invalid_data(self):
+        invalid_data = {
+            "title": ""
+        }
+        previous_count = QuestionBim.objects.count()
+        response = self.client.post(self.url, data=invalid_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(previous_count, QuestionBim.objects.count())
