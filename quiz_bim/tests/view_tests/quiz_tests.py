9b46adceaca0ea7d700ea6c661394dc28cf20dbd
Emir <malcikclashofclans@gmail.com>
2023-09-24 14:51:14 +0600

    #112, #113, #114 Базовые тесты для CreateView Написал базовые тесты для QuizBimCreateView, QuestionBimCreateView, AnswerBimCreateView

--- quiz_bim/tests/view_tests/quiz_tests.py
+++ quiz_bim/tests/view_tests/quiz_tests.py
@@ -16,7 +16,7 @@ class TestQuizBimListView(TestCase):
     def test_list_view(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertQuerysetEqual(response.context['tests'], QuizBim.objects.all())
+        self.assertQuerysetEqual(response.context['tests'], QuizBim.objects.all(), ordered=False)
         self.assertTemplateUsed(response, 'quiz_bim/quiz_bim/quiz_bim_list.html')
         self.assertFalse(response.context['is_paginated'])
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
@@ -35,3 +35,30 @@ class TestQuizBimDetailView(TestCase):
     def test_not_found(self):
         response = self.client.get(reverse("quiz_bim:test_detail", kwargs={"pk": 999}))
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+
+class TestQuizBimCreateView(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.correct_form_data = {
+            "title": "Quiz",
+            "questions_qty": 5,
+        }
+        cls.url = reverse("quiz_bim:test_create")
+        super().setUpClass()
+
+    def test_create_view(self):
+        previous_count = QuizBim.objects.count()
+        response = self.client.post(self.url, data=self.correct_form_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(QuizBim.objects.count() - previous_count, 1)
+
+    def test_invalid_data(self):
+        invalid_data = {
+            "title": "",
+            "questions_qty": "string",
+        }
+        previous_count = QuizBim.objects.count()
+        response = self.client.post(self.url, data=invalid_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(previous_count, QuizBim.objects.count())
