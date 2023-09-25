099a0813544c23642019a8003eb671e347314b89
Artur Sheshukov <arti.shishka11@gmail.com>
2023-09-25 21:00:39 +0600

    #174 Придумал решение создания постоянных миграций

--- subscription/models/user_subscription.py
+++ subscription/models/user_subscription.py
@@ -2,13 +2,8 @@ from datetime import datetime, timedelta
 
 from django.contrib.auth import get_user_model
 from django.db import models
-
 from modules.models import AbstractModel
 from subscription.models import SubscriptionModel
-
-
-def get_deadline():
-    return datetime.now() + timedelta(days=30)
 
 
 class UsersSubscription(AbstractModel):
@@ -16,10 +11,8 @@ class UsersSubscription(AbstractModel):
                                      verbose_name='Подписки')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='us_users',
                              verbose_name='Пользователи')
-    end_time = models.DateTimeField(default=get_deadline(), verbose_name='Дата окончания подписки')
+    end_time = models.DateTimeField(verbose_name='Дата окончания подписки')
     is_active = models.BooleanField(default=True, null=False, blank=False, verbose_name="Активность подписки")
 
     def __str__(self):
         return f'{self.pk}, {self.subscription}, {self.user}, {self.end_time}, {self.is_active}'
-
-
