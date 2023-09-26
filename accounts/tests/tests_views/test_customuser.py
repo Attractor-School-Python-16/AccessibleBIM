88c8cfbdb05dd53436e53063f58a8f23f95341ec
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-26 14:58:40 +0600

    #206 accounts test fix Поправила тесты, чтобы они соответствовали новой модели юзера и видоизменила ссылки

--- accounts/tests/tests_views/test_customuser.py
+++ accounts/tests/tests_views/test_customuser.py
@@ -16,6 +16,7 @@ class TestCustomUser(TestCase):
             'email': 'my_user@bimtest.com',
             'password1': '123',
             'password2': '123',
+            'country': 'KG',
             'phone_number': '+996555555555',
             'job_title': '',
             'type_corp': '7',
@@ -27,9 +28,9 @@ class TestCustomUser(TestCase):
     def test_register(self):
         form = RegisterForm(self.correct_data)
         self.assertTrue(form.is_valid())
-        response = self.client.post("/register/", data=self.correct_data)
+        response = self.client.post(reverse('accounts:register'), data=self.correct_data)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(response, reverse('accounts:verification_sent'))
+        self.assertRedirects(response, reverse('accounts:verification_sent')+'?email=my_user@bimtest.com')
         self.assertEqual(CustomUser.objects.count(), 1)
         self.assertEqual(CustomUser.objects.first().first_name, self.correct_data['first_name'])
         self.assertEqual(CustomUser.objects.first().last_name, self.correct_data['last_name'])
@@ -37,19 +38,15 @@ class TestCustomUser(TestCase):
         self.assertEqual(CustomUser.objects.first().email_verified, False)
 
     def test_change_password(self):
-        response = self.client.post("/register/", data=self.correct_data)
-        print('Response:', response)
+        self.client.post(reverse('accounts:register'), data=self.correct_data)
         logged_in = self.client.login(email='my_user@bimtest.com', password='123')
-        print('Logged in:', logged_in)
-        print('user: ', self.client)
         self.assertTrue(logged_in)
         data = {
             'old_password': '123',
             'new_password1': '000',
             'new_password2': '000'
         }
-        response = self.client.post("/password-change/", data=data)
-        print('response:', response)
+        response = self.client.post(reverse('accounts:change_password'), data=data)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.client.logout()
         self.assertFalse(self.client.login(email='my_user@bimtest.com', password='123'))
