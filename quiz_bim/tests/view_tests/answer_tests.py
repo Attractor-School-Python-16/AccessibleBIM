8d4faf83ee93c76e6c136d0e4812a1d1912ad38c
Emir <malcikclashofclans@gmail.com>
2023-09-26 23:18:19 +0600

    #223 Добавил суперпользователя во всех тестах Создал суперпользователя во всех тесткейсах Залогинил клиента с помощью декоратора

--- quiz_bim/tests/view_tests/answer_tests.py
+++ quiz_bim/tests/view_tests/answer_tests.py
@@ -1,13 +1,16 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from quiz_bim.models import AnswerBim
 from quiz_bim.tests.factories import QuestionBimFactory, AnswerBimFactory
+from quiz_bim.tests.urils import login_superuser_test
 
 
 class TestAnswerBimCreateView(TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls.correct_form_data = {
@@ -16,8 +19,10 @@ class TestAnswerBimCreateView(TestCase):
         }
         question = QuestionBimFactory.create()
         cls.url = reverse("quiz_bim:answer_create", kwargs={"pk": question.pk})
+        cls.superuser, _ = get_user_model().objects.get_or_create(email="admin@admin.com", password="admin", is_superuser=True)
         super().setUpClass()
 
+    @login_superuser_test
     def test_create_view(self):
         previous_count = AnswerBim.objects.count()
         response = self.client.post(self.url, data=self.correct_form_data)
@@ -26,6 +31,7 @@ class TestAnswerBimCreateView(TestCase):
         answer = AnswerBim.objects.latest('create_at')
         self.assertRedirects(response, reverse("quiz_bim:question_detail", kwargs={"pk": answer.question_bim.pk}))
 
+    @login_superuser_test
     def test_invalid_data(self):
         invalid_data = {
             "answer": "",
@@ -40,10 +46,16 @@ class TestAnswerBimCreateView(TestCase):
 class TestAnswerBimUpdateView(TestCase):
     answer = None
 
+    @classmethod
+    def setUpClass(cls):
+        cls.superuser, _ = get_user_model().objects.get_or_create(email="admin@admin.com", password="admin", is_superuser=True)
+        super().setUpClass()
+
     def setUp(self) -> None:
         self.answer = AnswerBimFactory.create()
         self.url = reverse("quiz_bim:answer_update", kwargs={"pk": self.answer.pk})
 
+    @login_superuser_test
     def test_update_view(self):
         new_data = {
             "answer": "New title",
@@ -56,6 +68,7 @@ class TestAnswerBimUpdateView(TestCase):
         self.assertEqual(self.answer.is_correct, False)
         # self.assertRedirects(response, reverse("quiz_bim:tests_list"))
 
+    @login_superuser_test
     def test_invalid_data(self):
         invalid_data = {
             "title": "",
@@ -64,17 +77,23 @@ class TestAnswerBimUpdateView(TestCase):
         response = self.client.post(self.url, data=invalid_data)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    @login_superuser_test
     def test_not_found(self):
         response = self.client.get(reverse("quiz_bim:answer_update", kwargs={"pk": 999}))
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
 
 class TestAnswerBimDeleteView(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.superuser, _ = get_user_model().objects.get_or_create(email="admin@admin.com", password="admin", is_superuser=True)
+        super().setUpClass()
 
     def setUp(self) -> None:
         self.answer = AnswerBimFactory.create()
         self.url = reverse("quiz_bim:answer_delete", kwargs={"pk": self.answer.pk})
 
+    @login_superuser_test
     def test_delete_view(self):
         previous_count = AnswerBim.objects.count()
         response = self.client.post(self.url)
@@ -82,6 +101,7 @@ class TestAnswerBimDeleteView(TestCase):
         self.assertEqual(previous_count - AnswerBim.objects.count(), 1)
         # self.assertRedirects(response, reverse("quiz_bim:tests_list"))
 
+    @login_superuser_test
     def test_not_found(self):
         previous_count = AnswerBim.objects.count()
         response = self.client.post(reverse("quiz_bim:answer_delete", kwargs={"pk": 999}))
