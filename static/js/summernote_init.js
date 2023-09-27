17b905d6881aa412886a5b9c78407e30cea44514
kumushai_urmanbetova <urmanbetovak@gmail.com>
2023-09-27 16:04:55 +0600

    Настроила summernote, добавила шрифты из ТЗ.

---
+++ static/js/summernote_init.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function() {
+    $('#description').summernote({
+        fontNames: ['Arial Nova Light', 'Arial Nova'],
+        fontNamesIgnoreCheck: ['Arial Nova Light', 'Arial Nova']
+    });
+});
