f425a2303c8e16791979ad8ce4d75a9e4e49bc0d
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-28 22:09:10 +0600

    #239 CurrencyRate Изменила модель CurrencyRate

--- currency/models/currency_rate.py
+++ currency/models/currency_rate.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CurrencyChoices(models.TextChoices):
+    USD = 'USD', _("USD")
+    RUB = 'RUB', _("RUB")
 
 
 class CurrencyRateModel(models.Model):
-    base_currency = models.CharField(max_length=3, verbose_name='Валюта')
-    goal_currency = models.CharField(max_length=3, verbose_name='Валюта')
+    base_currency = models.PositiveIntegerField(choices=CurrencyChoices.choices, verbose_name='Валюта')
+    goal_currency = models.PositiveIntegerField(choices=CurrencyChoices.choices, verbose_name='Валюта')
     rate = models.DecimalField(max_digits=18, decimal_places=8)
     created_at = models.DateTimeField(verbose_name='Cоздано', auto_now_add=True)
     source = models.CharField(max_length=500, verbose_name='Источник')
