c67c140b52748cace5985da81f94c68e27244c8b
Raim <raimdzhan.latypov@gmail.com>
2023-09-28 22:19:51 +0600

    #167 при загрузке видео добавил проверку на тип контента. Переопределил метод clean_video_file.

--- step/forms/video_form.py
+++ step/forms/video_form.py
@@ -2,6 +2,9 @@ from django import forms
 
 from step.models import VideoModel
 
+CONTENTTYPES = ['video/mp4',
+                'video/x-msvideo']
+
 
 class VideoForm(forms.ModelForm):
     class Meta:
@@ -12,3 +15,10 @@ class VideoForm(forms.ModelForm):
             'video_description': 'Введите описание видео',
             'video_file': 'Загрузите видео-файл',
         }
+
+    def clean_video_file(self):
+        current_file = self.cleaned_data.get("video_file", False)
+        if current_file.content_type in CONTENTTYPES:
+            return current_file
+        else:
+            raise forms.ValidationError("Необходимо загрузить видео в формате MP4 или AVI")
