ee00d9c2ac213d5ff9638ed97d9708d276eb250f
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-29 00:13:18 +0600

    #239 exchange api Немного изменила модель CurrencyRate, настроила подключение к api и запись данных в бд

--- currency/models/currency_rate.py
+++ currency/models/currency_rate.py
@@ -8,11 +8,12 @@ class CurrencyChoices(models.TextChoices):
 
 
 class CurrencyRateModel(models.Model):
-    base_currency = models.PositiveIntegerField(choices=CurrencyChoices.choices, verbose_name='Валюта')
-    goal_currency = models.PositiveIntegerField(choices=CurrencyChoices.choices, verbose_name='Валюта')
+    base_currency = models.CharField(max_length=3, choices=CurrencyChoices.choices, verbose_name='Валюта')
+    goal_currency = models.CharField(max_length=3, choices=CurrencyChoices.choices, verbose_name='Валюта')
     rate = models.DecimalField(max_digits=18, decimal_places=8)
     created_at = models.DateTimeField(verbose_name='Cоздано', auto_now_add=True)
     source = models.CharField(max_length=500, verbose_name='Источник')
+    timestamp = models.DateTimeField(verbose_name='Дата')
 
     def __str__(self):
         return f'{self.base_currency}{self.goal_currency} {self.rate} at {self.created_at}'
