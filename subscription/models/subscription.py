fff0cbc68ba06089743459a631183948d7d89828
Anastassiya <anastassiya.belogurova@gmail.com>
2023-09-29 23:47:30 +0600

    #232 add is_published Добавила поле is_published в модель Subsciption, обновила форму, добавила миграцию.

--- subscription/models/subscription.py
+++ subscription/models/subscription.py
@@ -8,6 +8,7 @@ class SubscriptionModel(AbstractModel):
     course = models.ForeignKey('modules.CourseModel', on_delete=models.PROTECT, related_name='courses',
                                verbose_name='Курс')
     price = models.IntegerField(null=False, blank=False, verbose_name='Цена за курс')
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
     user_subscription = models.ManyToManyField(get_user_model(), through="UsersSubscription")
 
 
