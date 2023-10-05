e967c0fdd0bf5c722eb9f387cacd5b490e6c8e15
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-05 10:38:30 +0600

    #165 accounts unittests Переделала фабрику для юзера (юнит тестирование)

--- accounts/tests/factories/user.py
+++ accounts/tests/factories/user.py
@@ -8,15 +8,17 @@ class UserFactory(DjangoModelFactory):
     first_name = factory.Sequence(lambda t: f"User{t}")
     last_name = factory.Sequence(lambda t: f"User{t}")
     father_name = ""
-    password1 = '123'
-    password2 = '123'
+    email = factory.Sequence(lambda t: f"user{t}@bimtest.com")
+    password = '123'
+    # password1 = '123'
+    # password2 = '123'
+    country = 'KG'
     phone_number = '+996555555555'
     job_title = ''
     company = ''
     type_corp = '7'
-    email = factory.Sequence(lambda t: f"user{t}@bimtest.com")
-    captcha_0 = 'dummy-value'
-    captcha_1 = 'PASSED'
+    # captcha_0 = 'dummy-value'
+    # captcha_1 = 'PASSED'
 
     class Meta:
         model = CustomUser
