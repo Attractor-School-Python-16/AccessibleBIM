c94f8f12c6571812571acb27f4a5be190a528ec5
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-06 14:06:50 +0600

    #277 statistics users Добавила приложение reports, в нем создала вью с отображением карточек. Настроила эндпоинты.

---
+++ reports/views/users_statistics_views/users_main_statistics_view.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.views.generic import TemplateView
+
+from subscription.models.user_subscription import UsersSubscription
+
+
+class UsersMainStatisticsView(TemplateView):
+    template_name = 'statistics/users_statistics/users_statistics_main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['registered_users_qty'] = get_user_model().objects.count()
+        context['active_subscribers_qty'] = UsersSubscription.objects.filter(is_active=True).count()
+        # Должно быть UsersSubscription.objects.filter(is_active=True).distinct('user').count()
+        # Но работает только с postgres, не с sqlite
+        return context
