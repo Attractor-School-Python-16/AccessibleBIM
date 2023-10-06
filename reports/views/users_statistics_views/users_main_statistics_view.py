702f2afa2f867b25f47c78cb83b2c18dbd8fb085
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-06 15:05:24 +0600

    #277 statistics users main Добавила график: кол-во новых юзеров по периодам времени

--- reports/views/users_statistics_views/users_main_statistics_view.py
+++ reports/views/users_statistics_views/users_main_statistics_view.py
@@ -8,7 +8,7 @@ class UsersMainStatisticsView(TemplateView):
     template_name = 'statistics/users_statistics/users_statistics_main.html'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data()
+        context = super().get_context_data(**kwargs)
         context['registered_users_qty'] = get_user_model().objects.count()
         context['active_subscribers_qty'] = UsersSubscription.objects.filter(is_active=True).count()
         # Должно быть UsersSubscription.objects.filter(is_active=True).distinct('user').count()
