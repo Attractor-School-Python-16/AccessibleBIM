a656034ecfac6597877448ed68cadf2254d60ad6
arturateo <arti.shishka11@gmail.com>
2023-10-10 23:26:58 +0600

    #267 Реализовал библеотеку HTMX в приложении Answer_bim

--- quiz_bim/urls.py
+++ quiz_bim/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from quiz_bim.views.answer_bim_views import AnswerBimCreateView, AnswerBimUpdateView, AnswerBimDeleteView
-from quiz_bim.views.question_bim_views import QuestionBimDetailView, QuestionBimUpdateView, \
-    QuestionBimDeleteView
+from quiz_bim.views.answer_bim_views import AnswerBimFormDetailView, AnswerBimFormCreateView, AnswerBimFormUpdateView, \
+    AnswerBimFormDeleteView
+from quiz_bim.views.question_bim_views import QuestionBimFormDetailView, QuestionBimFormCreateView, \
+    QuestionBimFormUpdateView, QuestionBimFormDeleteView
 from quiz_bim.views.take_quiz_views import TakeQuizView, QuestionsCompletionView, UserAnswerAPIView, QuizResultView
 from quiz_bim.views.quiz_bim_views import QuizBimListView, QuizBimCreateView, QuizBimDetailView, QuizBimUpdateView, \
-    QuizBimDeleteView, QuestionBimFormDetailView, QuestionBimFormCreateView, QuestionBimFormDeleteView, \
-    QuestionBimFormUpdateView
+    QuizBimDeleteView
 
 app_name = 'quiz_bim'
 
@@ -25,21 +25,13 @@ urlpatterns = [
     path('test/<int:tpk>/question_bim/delete/<int:qpk>/', QuestionBimFormDeleteView.as_view(),
          name='questionbim_htmx_delete'),
 
-    path('question_bim/<int:pk>/', QuestionBimDetailView.as_view(), name='questionbim_detail'),
-    path('question_bim/<int:pk>/update/', QuestionBimUpdateView.as_view(), name='questionbim_update'),
-    path('question_bim/<int:pk>/delete/', QuestionBimDeleteView.as_view(), name='questionbim_delete'),
-
-    path('question_bim/<int:pk>/answer_bim/create/', AnswerBimCreateView.as_view(), name='answerbim_create'),
-    path('answer_bim/<int:pk>/update/', AnswerBimUpdateView.as_view(), name='answerbim_update'),
-    path('answer_bim/<int:pk>/delete/', AnswerBimDeleteView.as_view(), name='answerbim_delete'),
-
-    # path('answer_bim/detail/<int:pk>/', AnswerBimFormDetailView.as_view(),
-    #      name='answerbim_htmx_detail'),
-    # path('answer_bim/create/', AnswerBimFormCreateView.as_view(), name='answerbim_htmx_create'),
-    # path('answer_bim/update/<int:pk>/', AnswerBimFormUpdateView.as_view(),
-    #      name='answerbim_htmx_update'),
-    # path('answer_bim/delete/<int:pk>/', AnswerBimFormDeleteView.as_view(),
-    #      name='answerbim_htmx_update'),
+    path('question_bim/<int:qpk>/answer_bim/detail/<int:apk>/', AnswerBimFormDetailView.as_view(),
+         name='answerbim_htmx_detail'),
+    path('question_bim/<int:pk>/answer_bim/create/', AnswerBimFormCreateView.as_view(), name='answerbim_htmx_create'),
+    path('question_bim/<int:qpk>/answer_bim/update/<int:apk>/', AnswerBimFormUpdateView.as_view(),
+         name='answerbim_htmx_update'),
+    path('question_bim/<int:qpk>/answer_bim/delete/<int:apk>/', AnswerBimFormDeleteView.as_view(),
+         name='answerbim_htmx_delete'),
 
     path('take-test/<int:pk>/', TakeQuizView.as_view(), name='take_test'),
     path('test-completion/<int:pk>/', QuestionsCompletionView.as_view(), name='test_completion'),
