5c03560733d673a6f2d01e978045b44e2ff585b1
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-12 00:36:48 +0600

    #278 statistics users list Перенесла таблицу юзеров с главной страницы статистики на страницу юзеров.

--- reports/urls.py
+++ reports/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from reports.views import UsersMainStatisticsView
+from reports.views import UsersMainStatisticsView, UsersListStatisticsView
 from reports.views.users_statistics_views.api import get_new_users_gty_view
 
 app_name = 'reports'
 
 urlpatterns = [
     path('users/', UsersMainStatisticsView.as_view(), name='stats_users_main'),
+    path('users-list/', UsersListStatisticsView.as_view(), name='stats_users_list'),
 
     path('get-new-users-qty/', get_new_users_gty_view, name='get_new_users_qty'),
 ]
