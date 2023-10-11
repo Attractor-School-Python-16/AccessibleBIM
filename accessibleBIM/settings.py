96a1caff1384a37d7ea883334ef9aebf9bf63617
Emir <malcikclashofclans@gmail.com>
2023-10-11 15:42:14 +0600

    #257 Добавил форму после авторизации. И также поправил кодек фикстур.

--- accessibleBIM/settings.py
+++ accessibleBIM/settings.py
@@ -166,6 +166,8 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
+SOCIALACCOUNT_AUTO_SIGNUP = False
+ACCOUNT_SIGNUP_FORM_CLASS = 'accounts.forms.CustomSignupForm'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
