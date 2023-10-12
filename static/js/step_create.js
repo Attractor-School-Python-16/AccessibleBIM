92bc20c70a843ab9f3da96ce2765cb3af3ce6de5
RustemGaliulin <asmoderous@gmail.com>
2023-10-13 01:38:41 +0600

    #267 шаблон создания step добавил js код для обработки выбора существующего видео с отключением инпутов для создания нового добавил проверки на тип урока

--- static/js/step_create.js
+++ static/js/step_create.js
@@ -1,9 +1,6 @@
-const fileSelect = document.getElementById("id_step-file");
 const videoSelect = document.getElementById("id_step-video")
 const textSelect = document.getElementById("id_step-text")
 const testSelect = document.getElementById("id_step-test")
-
-
 
 
 function handleSelectChange(select, type) {
@@ -21,157 +18,42 @@ function handleSelectChange(select, type) {
             contentDiv.style.pointerEvents = "auto"
 
         }
+    } else if (type === "video") {
+        let inputTitle = document.getElementById("id_video-video_title");
+        let inputDescription = document.getElementById("id_video-video_description");
+        let inputVideoFile = document.getElementById("id_video-video_file");
+        if (select.value) {
+            inputTitle.setAttribute("disabled", "disabled");
+            inputDescription.setAttribute("disabled", "disabled");
+            inputVideoFile.setAttribute("disabled", "disabled");
+        } else {
+            inputTitle.removeAttribute("disabled");
+            inputDescription.removeAttribute("disabled");
+            inputVideoFile.removeAttribute("disabled");
+        }
+
+
     }
 }
 
 
-
-textSelect.addEventListener("change", function () {
-    handleSelectChange(textSelect, "text");
-});
-
-videoSelect.addEventListener("change", function () {
-    handleSelectChange(videoSelect, videoForm);
-});
-
-testSelect.addEventListener("change", function () {
-    handleSelectChange(testSelect, testForm);
-});
-
-lessonType.addEventListener("change", function () {
-    if (lessonType.value === "text") {
-        textContent.style.display = "block";
-        videoContent.style.display = "none";
-        testContent.style.display = "none";
-        clearInputsInDiv(videoForm);
-        clearInputsInDiv(testForm);
-    } else if (lessonType.value === "video") {
-        textContent.style.display = "none";
-        videoContent.style.display = "block";
-        testContent.style.display = "none";
-        clearInputsInDiv(textForm);
-        clearInputsInDiv(testForm);
-    } else if (lessonType.value === "test") {
-        textContent.style.display = "none";
-        videoContent.style.display = "none";
-        testContent.style.display = "block";
-        clearInputsInDiv(textForm);
-        clearInputsInDiv(videoForm);
-    }
-});
-
-document.getElementById("confirmQuestions").addEventListener("click", function () {
-    const questionsQtyInput = document.getElementById("questions_qty_to_create")
-    const questionsQty = parseInt(questionsQtyInput.value);
-    if (!isNaN(questionsQty)) {
-        createQuestionInputs(questionsQty);
-    }
-});
-
-let questionCounter = 1;
-
-function createQuestionInputs(questionsQty) {
-    const questionForm = document.getElementById("question-form");
-    const questionBlocksCountInput = document.getElementById("questionBlocksCount");
-    questionForm.innerHTML = "";
-
-    for (let i = 1; i <= questionsQty; i++) {
-        const questionBlock = document.createElement("div");
-        questionBlock.setAttribute("class", "question-block");
-        questionBlock.setAttribute("display", "none");
-
-        const questionLabel = document.createElement("label");
-        questionLabel.setAttribute("for", `question_title_${i}`);
-        questionLabel.textContent = `Введите вопрос ${i}`;
-
-        const questionInput = document.createElement("input");
-        questionInput.setAttribute("type", "text");
-        questionInput.setAttribute("name", `question_title_${i}`);
-        questionInput.setAttribute("class", "form-control");
-        questionInput.setAttribute("required", "required");
-
-        const answersContainer = document.createElement("div");
-        answersContainer.setAttribute("class", "content-test");
-
-        const answersQtyInput = document.createElement("input");
-        answersQtyInput.setAttribute("type", "hidden");
-        answersQtyInput.setAttribute("name", `answers_qty_${i}`);
-        answersQtyInput.value = 0;
-
-        const addAnswerButton = document.createElement("button");
-        addAnswerButton.setAttribute("type", "button");
-        addAnswerButton.setAttribute("class", "btn btn-primary my-3");
-        addAnswerButton.textContent = "Добавить ответ";
-        addAnswerButton.setAttribute("data-question-number", i);
-
-        addAnswerButton.addEventListener("click", function () {
-            const questionNumber = this.getAttribute("data-question-number");
-            createAnswerInputs(answersContainer, questionNumber);
-        });
-
-        questionBlock.appendChild(questionLabel);
-        questionBlock.appendChild(questionInput);
-        questionBlock.appendChild(answersContainer);
-        questionBlock.appendChild(answersQtyInput);
-        questionBlock.appendChild(addAnswerButton);
-
-        questionForm.appendChild(questionBlock);
-
-        questionCounter++;
-        questionBlocksCountInput.value = parseInt(questionBlocksCountInput.value) + 1;
-    }
+if (textSelect) {
+    textSelect.addEventListener("change", function () {
+        handleSelectChange(textSelect, "text");
+    });
 }
 
-function createAnswerInputs(answersContainer, questionNumber) {
-    const answerBlock = document.createElement("div");
-    answerBlock.setAttribute("class", "content-test");
-
-    const questionIndex = questionNumber;
-    const answersQtyInput = answersContainer.parentElement.querySelector(`input[name="answers_qty_${questionIndex}"]`);
-    const currentAnswersQty = parseInt(answersQtyInput.value, 10);
-
-    const answerLabel = document.createElement("label");
-    answerLabel.setAttribute("for", `answer_${questionNumber}_${currentAnswersQty + 1}`);
-    answerLabel.textContent = `Введите ответ ${currentAnswersQty + 1} для вопроса ${questionNumber}`;
-
-    const answerInput = document.createElement("input");
-    answerInput.setAttribute("type", "text");
-    answerInput.setAttribute("name", `answer_${questionNumber}_${currentAnswersQty + 1}`);
-    answerInput.setAttribute("class", "form-control");
-    // answerInput.setAttribute("required", "required");
-
-    const isCorrectLabel = document.createElement("label");
-    isCorrectLabel.textContent = "Выберите правильность ответа";
-
-    const isCorrectSelect = document.createElement("select");
-    isCorrectSelect.setAttribute("name", `is_correct_${questionNumber}_${currentAnswersQty + 1}`);
-    isCorrectSelect.setAttribute("class", "form-control");
-    isCorrectSelect.innerHTML = `
-            <option value="False" selected>Неверный ответ</option>
-            <option value="True">Верный ответ</option>
-        `;
-
-    answerBlock.appendChild(answerLabel);
-    answerBlock.appendChild(answerInput);
-    answerBlock.appendChild(isCorrectLabel);
-    answerBlock.appendChild(isCorrectSelect);
-
-    answersContainer.appendChild(answerBlock);
-
-    answersQtyInput.value = currentAnswersQty + 1;
+if (videoSelect) {
+    videoSelect.addEventListener("change", function () {
+        handleSelectChange(videoSelect, "video");
+    });
 }
 
-const questionsQtyInput = document.getElementById("questions_qty_to_create");
-const confirmQuestionsButton = document.getElementById("confirmQuestions");
-const paginationBlock = document.getElementById("pagination-block");
-
-confirmQuestionsButton.addEventListener("click", function () {
-    const questionsQty = parseInt(questionsQtyInput.value);
-
-    generatePagination(questionsQty);
-
-    showQuestionBlock(1);
-});
+if (testSelect) {
+    testSelect.addEventListener("change", function () {
+        handleSelectChange(testSelect, testForm);
+    });
+}
 
 
 function generatePagination(numPages) {
@@ -195,32 +77,3 @@ function generatePagination(numPages) {
         });
     }
 }
-
-function showQuestionBlock(pageNumber) {
-    const questionBlocks = document.querySelectorAll(".question-block");
-    currentPage = pageNumber;
-
-    questionBlocks.forEach((block, index) => {
-        if (index === pageNumber - 1) {
-            block.style.display = "block";
-        } else {
-            block.style.display = "none";
-        }
-    });
-}
-
-const questionBlocksCount = document.getElementById("questions_qty_to_create");
-const questionsQty = document.getElementById("questions_qty_test");
-const sendButton = document.getElementById("submit_button");
-
-function handleInputChange() {
-    if (parseInt(questionBlocksCount.value) < parseInt(questionsQty.value)) {
-        alert('Количество создаваемых вопросов не может быть меньше количества вопросов в тесте');
-        sendButton.disabled = true;
-    } else {
-        sendButton.disabled = false;
-    }
-}
-
-questionBlocksCount.addEventListener('change', handleInputChange);
-questionsQty.addEventListener('change', handleInputChange);
