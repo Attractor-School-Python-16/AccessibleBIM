1d4c793fa52afc0ea331f1b4ae844f31157af7f7
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-13 21:58:54 +0600

    #280 statistics courses main Добавила график для отображения пройденных уроков

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from reports.views import UsersMainStatisticsView, CoursesMainStatisticsView
-from reports.views.users_statistics_views.api import get_new_users_gty_view
+from reports.views.courses_statistics_views.api import get_steps_completed_qty_view
+from reports.views.users_statistics_views.api import get_new_users_qty_view
 
 app_name = 'reports'
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('users/', UsersMainStatisticsView.as_view(), name='stats_users_main'),
     path('courses/', CoursesMainStatisticsView.as_view(), name='stats_courses_main'),
 
-    path('get-new-users-qty/', get_new_users_gty_view, name='get_new_users_qty'),
+    path('get-new-users-qty/', get_new_users_qty_view, name='get_new_users_qty'),
+    path('get-steps-completed-qty/', get_steps_completed_qty_view, name='get_steps_completed_qty'),
 ]
