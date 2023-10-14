1fa61ca11dc18e56ea54cd7d8907069ec8990b2b
Emir <malcikclashofclans@gmail.com>
2023-10-14 21:35:43 +0600

    #257 Создал статические страницы. И сделал base.html для них

--- static_pages/views.py
+++ static_pages/views.py
@@ -3,4 +3,16 @@ from django.views.generic import TemplateView
 
 
 class AccessibleBIM(TemplateView):
-    template_name = "static_pages/accessibleBIM.html"
+    template_name = "static_pages/accessible-bim.html"
+
+
+class About(TemplateView):
+    template_name = "static_pages/about.html"
+
+
+class Contacts(TemplateView):
+    template_name = "static_pages/contacts.html"
+
+
+class PrivacyPolicy(TemplateView):
+    template_name = "static_pages/privacy-policy.html"
