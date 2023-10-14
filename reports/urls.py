3d8b6325cd122af9091e8299faf7f2f9ec8c9f76
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-14 21:34:04 +0600

    #279 statistics user detailed Добавила бар чарт для отслеживания прохождения тестов в курсах

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from reports.views import UsersMainStatisticsView, UsersListStatisticsView, UserDetailedStatisticsView
-from reports.views.users_statistics_views.api import get_new_users_gty_view
+from reports.views.users_statistics_views.api import get_new_users_gty_view, get_test_progress_view
 
 app_name = 'reports'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('user/<int:pk>/', UserDetailedStatisticsView.as_view(), name='stats_user_detailed'),
 
     path('get-new-users-qty/', get_new_users_gty_view, name='get_new_users_qty'),
+    path('get-test-progress/', get_test_progress_view, name='get_test_progress'),
 ]
