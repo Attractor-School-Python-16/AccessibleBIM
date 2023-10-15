8a25225c4ed45c28f12ee6f7780bceadfa26e58d
RustemGaliulin <asmoderous@gmail.com>
2023-10-15 22:41:59 +0600

    #267 валидация редактирования ответов переопределил порядок валиации и передаваемых данных, реализовал те же валидации для ответов, что и при создании

--- quiz_bim/validators.py
+++ quiz_bim/validators.py
@@ -1,26 +1,24 @@
 
-def validate_answer(form, question):
-    return check_for_errors(form, question)
+def validate_answer(question, answer, is_correct):
+    return check_for_errors(question,  answer, is_correct)
 
-def check_for_errors(form, question):
+def check_for_errors(question, answer, is_correct):
     error_messages = []
-    fields_validation = check_fields(form)
-    for i in fields_validation:
-        error_messages.append(i)
-    error_messages.append(check_correct_answers_count(form, question))
+    fields_validation = check_fields(answer, is_correct)
+    error_messages += fields_validation
+    error_messages.append(check_correct_answers_count(question, is_correct))
     return [i for i in error_messages if i is not None and i]
 
 
-def check_correct_answers_count(form, question):
+def check_correct_answers_count(question, is_correct):
     answers_count = question.answer_bim.count()
     correct_answers = question.answer_bim.all().filter(is_correct=True)
-    instance_is_correct = form.is_correct
     if answers_count >= 3:
-        if not correct_answers and not instance_is_correct:
+        if not correct_answers and not is_correct:
             error_message = ("В вопросе должен быть хотя бы один правильный ответ")
             return error_message
     if answers_count >= 2:
-        if len(correct_answers) == 1 and instance_is_correct:
+        if len(correct_answers) == 1 and is_correct:
             error_message = ("В вопросе не может быть более двух правильных ответов")
             return error_message
     return None
@@ -29,10 +27,8 @@ def check_correct_answers_count(form, question):
 #проверка полей проводится здесь же, поскольку ValidationError и self.add_errors в форме не работают (форма не
 # отображается), вероятно потому что не передается pk вопроса
 
-def check_fields(form):
+def check_fields(answer, is_correct):
     errors = []
-    answer = form.answer
-    is_correct = form.is_correct
     if not answer:
         errors.append("Поле ответа на может быть пустым")
     if len(answer) < 2:
