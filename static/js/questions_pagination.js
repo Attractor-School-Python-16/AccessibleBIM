7f3baa81f5b30dd4da8b5e0070878fb9993dec70
RustemGaliulin <asmoderous@gmail.com>
2023-10-15 14:24:55 +0600

    #267 шаблон для создания вопросов починил пагинацию при создании нескольких вопросов

--- static/js/questions_pagination.js
+++ static/js/questions_pagination.js
@@ -1,68 +1,74 @@
-        let questionBlocks = document.querySelectorAll('.question-block');
-        let paginationContainer = document.getElementById('pagination-block');
+let questionBlocks = document.querySelectorAll('.question-block');
+let paginationContainer = document.getElementById('pagination-block');
 
-        let currentPage = 0;
+let currentPage = 0;
 
-        function showQuestions(pageNumber) {
-            questionBlocks.forEach((block, index) => {
-                if (index === pageNumber) {
-                    block.style.display = 'block';
-                } else {
-                    block.style.display = 'none';
-                }
-            });
+function showQuestions(pageNumber) {
+    questionBlocks.forEach((block, index) => {
+        if (index === pageNumber) {
+            block.style.display = 'block';
+        } else {
+            block.style.display = 'none';
         }
+    });
+}
 
-        function createPaginationButtons() {
-            paginationContainer.innerHTML = '';
+function createPaginationButtons() {
+    paginationContainer.innerHTML = '';
 
-            questionBlocks = document.querySelectorAll('.question-block');
+    questionBlocks = document.querySelectorAll('.question-block');
 
-            questionBlocks.forEach((_, index) => {
-                let button = document.createElement('li');
-                button.classList.add('page-item');
-                let buttonLink = document.createElement('a');
-                buttonLink.classList.add('page-link');
-                buttonLink.classList.add('mx-1');
-                buttonLink.type = "button";
-                buttonLink.textContent = index + 1;
+    questionBlocks.forEach((_, index) => {
+        let button = document.createElement('li');
+        button.classList.add('page-item');
+        let buttonLink = document.createElement('a');
+        buttonLink.classList.add('page-link');
+        buttonLink.classList.add('mx-1');
+        buttonLink.type = "button";
+        buttonLink.textContent = index + 1;
 
-                buttonLink.addEventListener('click', () => {
-                    showQuestions(index);
-                });
-
-                button.appendChild(buttonLink);
-                paginationContainer.appendChild(button);
-            });
-        }
-        
-        function updatePageAfterNewQuestionCreated() {
-            let questionForms = document.getElementById('questionforms');
-            let newQuestionBlock = document.createElement('div');
-            newQuestionBlock.classList.add('question-block');
-            newQuestionBlock.appendChild(questionForms.cloneNode(true));
-            let questionsContainer = document.querySelector('.questions-container');
-            questionsContainer.appendChild(newQuestionBlock);
-            questionForms.remove();
-            createPaginationButtons();
-            showQuestions(currentPage);
-        }
-
-        document.body.addEventListener('htmx:afterOnLoad', function (event) {
-            let questionCreated = document.getElementById("questionforms")
-            let questionToProcess = questionCreated.querySelector("#question_detail")
-            if (questionToProcess) {
-                updatePageAfterNewQuestionCreated()
-            }
+        buttonLink.addEventListener('click', () => {
+            showQuestions(index);
         });
 
-        let deleteButtons = document.querySelectorAll('[id="delete-question"]')
+        button.appendChild(buttonLink);
+        paginationContainer.appendChild(button);
+    });
+}
 
-        deleteButtons.forEach((button) => {
-            button.addEventListener('click', () => {
-                window.location.reload();
-            });
-        });
+function updatePageAfterNewQuestionCreated() {
+    let questionForms = document.getElementById('questionforms');
+    let questionDetailBlock = questionForms.querySelector('#question_detail');
+
+    if (questionDetailBlock) {
+        let newQuestionBlock = document.createElement('div');
+        newQuestionBlock.classList.add('question-block');
+        newQuestionBlock.appendChild(questionDetailBlock.cloneNode(true));
+        let questionsContainer = document.querySelector('.questions-container');
+        questionsContainer.appendChild(newQuestionBlock);
+
+        questionDetailBlock.remove();
 
         createPaginationButtons();
         showQuestions(currentPage);
+    }
+}
+
+document.body.addEventListener('htmx:afterOnLoad', function (event) {
+    let questionCreated = document.getElementById("questionforms")
+    let questionToProcess = questionCreated.querySelector("#question_detail")
+    if (questionToProcess) {
+        updatePageAfterNewQuestionCreated()
+    }
+});
+
+let deleteButtons = document.querySelectorAll('[id="delete-question"]')
+
+deleteButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        window.location.reload();
+    });
+});
+
+createPaginationButtons();
+showQuestions(currentPage);
