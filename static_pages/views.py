1bda7eb537619bcc0c1951eaa8a52b5dd2f7d369
Artur Sheshukov <arti.shishka11@gmail.com>
2023-10-17 20:11:37 +0600

    #271 Перенес и отрефакторил стаческие страницы в отдельную папку для frontа. Добавил в gitignore yml файлы так как вносил изменения для работы Dockerа

--- static_pages/views.py
+++ static_pages/views.py
@@ -3,16 +3,42 @@ from django.views.generic import TemplateView
 
 
 class AccessibleBIM(TemplateView):
-    template_name = "static_pages/accessible-bim.html"
+    template_name = "front/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'AccessibleBIM'
+        context['sub_title'] = 'БИМСТАНДАРД'
+        return context
 
 
 class About(TemplateView):
-    template_name = "static_pages/about.html"
+    template_name = "front/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'O нас'
+        context['sub_title'] = ('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error '
+                                'eum ex excepturi, odit recusandae voluptatum. Ducimus laborum maiores tenetur.')
+        return context
 
 
 class Contacts(TemplateView):
-    template_name = "static_pages/contacts.html"
+    template_name = "front/contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Свяжитесь с нами'
+        context[
+            'sub_title'] = ('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error eum ex excepturi,'
+                            ' odit recusandae voluptatum. Ducimus laborum maiores tenetur.')
+        return context
 
 
 class PrivacyPolicy(TemplateView):
-    template_name = "static_pages/privacy-policy.html"
+    template_name = "front/privacy-policy.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Политика конфиденциальности'
+        return context
