36cd8c486922adb950e692c1caf1a141fdfe37dd
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-18 22:43:08 +0600

    #283 statistics sales main Добавила в статистику по подпискам карточки с ценами и график с новыми подписками

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from reports.views import UsersMainStatisticsView, UsersListStatisticsView, UserDetailedStatisticsView, \
     CoursesMainStatisticsView, SalesMainStatisticsView
+from reports.views.sales_statistics_views.api import get_new_subscriptions_qty_view
 from reports.views.users_statistics_views.api import get_new_users_qty_view, get_test_progress_view
 from reports.views.courses_statistics_views.api import get_steps_completed_qty_view, get_lesson_types_view
 
@@ -19,4 +20,5 @@ urlpatterns = [
     path('get-steps-completed-qty/', get_steps_completed_qty_view, name='get_steps_completed_qty'),
     path('get-lesson-types/', get_lesson_types_view, name='get_lesson_types'),
     path('get-test-progress/', get_test_progress_view, name='get_test_progress'),
+    path('get-new-subscriptions-qty/', get_new_subscriptions_qty_view, name='get_new_subscriptions'),
 ]
