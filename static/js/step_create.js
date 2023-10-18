04a3c0e9c9e56accf68f22d64ba5548318f038a3
RustemGaliulin <asmoderous@gmail.com>
2023-10-18 21:53:59 +0600

    #267 создание/редактирование step добавил кнопку снятия выбора с файлов при создании и редактировании файлов

--- static/js/step_create.js
+++ static/js/step_create.js
@@ -3,6 +3,23 @@ const textSelect = document.getElementById("id_step-text")
 const testSelect = document.getElementById("id_step-test")
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("here")
+    let deselectButton = document.createElement("button");
+    deselectButton.textContent = "Снять выбор";
+    deselectButton.className = "btn btn-primary"
+    deselectButton.addEventListener("click", function(event) {
+        event.preventDefault()
+      let selectElement = document.getElementById('id_step-file');
+      for (let i = 0; i < selectElement.options.length; i++) {
+        selectElement.options[i].selected = false;
+      }
+    });
+    let fileDiv = document.getElementById('div_id_step-file');
+    fileDiv.parentNode.appendChild(deselectButton);
+  });
+
+
 function clearTextAreas() {
 
     const descFrame = document.getElementById("id_text-text_description_iframe");
@@ -88,3 +105,4 @@ if (testSelect) {
         handleSelectChange(testSelect, "test");
     });
 }
+
