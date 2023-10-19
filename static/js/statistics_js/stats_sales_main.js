820505d1c75bb5e7bc1dd88cff0a4cc83386a10f
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-19 22:31:29 +0600

    #283 statistics sales main Доработала js для отображения графиков за разные периоды

--- static/js/statistics_js/stats_sales_main.js
+++ static/js/statistics_js/stats_sales_main.js
@@ -9,8 +9,8 @@ async function makeRequest(url, method){
         return await response.json();
     } else {
         let error = new Error(response.statusText);
-        error.response = response
-        console.log(error)
+        error.response = response;
+        console.log(error);
         throw error;
     }
 }
@@ -21,7 +21,6 @@ async function getDataByDay(url, days){
 }
 
 async function renderChart(type, name){
-    chartUrlsByDays[name].url
     let response = await getDataByDay(chartUrlsByDays[name].url, 7);
     if (!response.error){
         let options = createChartOptions(type, response.labels, response.values);
@@ -34,7 +33,7 @@ async function renderChart(type, name){
 }
 
 async function updateChart(name, days){
-    let response = await getDataByDay(chartUrlsByDays[name].url, days)
+    let response = await getDataByDay(chartUrlsByDays[name].url, days);
     if (!response.error){
          window.charts[name].updateOptions({
            xaxis: {
@@ -47,7 +46,7 @@ async function updateChart(name, days){
 
     }
     else{
-        $(chartUrlsByDays[name].div).text('Error occured while loading data')
+        $(chartUrlsByDays[name].div).text('Error occured while loading data');
     }
 }
 
@@ -55,17 +54,22 @@ async function changeDaysOnChart(event){
     let btn = event.currentTarget;
     $(btn).siblings().removeClass("active");
     $(btn).addClass( "active" );
-    let days = $(btn).data('days')
-    await updateChart('newSubscriptionsChart', days)
+    let days = $(btn).data('days');
+    $.each(chartUrlsByDays, function (index){
+        if ($(btn).hasClass(this.btn_cls)){
+            chart = index;
+        }
+    })
+    await updateChart(chart, days);
 }
 
 
 function createChartOptions(type, labels, values){
     if (type === 'area'){
-        return createAreaChartOptions(labels, values)
+        return createAreaChartOptions(labels, values);
     }
     else if (type === 'bar'){
-        return createBarChartOptions(labels, values)
+        return createBarChartOptions(labels, values);
     }
 }
 
@@ -109,22 +113,25 @@ function createBarChartOptions(labels, values){
 let newSubscriptionsChartDiv = document.getElementById('new-subscriptions-chart');
 let popularCoursesChartDiv = document.getElementById('popular-courses-chart');
 
+let newSubscriptionButtons = $('.new-subscriptions-chart-btn');
+let popularCoursesButtons = $('.popular-courses-chart-btn');
+
 let chartUrlsByDays = {
     'newSubscriptionsChart': {
         url: '/statistics/get-new-subscriptions-qty/?days=',
         div: newSubscriptionsChartDiv,
+        btn_cls: 'new-subscriptions-chart-btn'
     },
     'popularCoursesChart': {
         url: '/statistics/get-popular-courses/?days=',
         div: popularCoursesChartDiv,
+        btn_cls: 'popular-courses-chart-btn'
     }
-}
+};
 
 
 renderChart('area', 'newSubscriptionsChart');
-let newSubscriptionButtons = $('.new-subscriptions-chart-btn')
-newSubscriptionButtons.click(changeDaysOnChart)
+newSubscriptionButtons.click(changeDaysOnChart);
 
 renderChart('bar', 'popularCoursesChart');
-let popularCoursesButtons = $('.popular-courses-chart-btn')
-popularCoursesButtons.click(changeDaysOnChart)
+popularCoursesButtons.click(changeDaysOnChart);
