3baa3c7d1d8f26249ee537d55933427ff82732cc
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-20 21:45:12 +0600

    #282 statistics course detailed Добавила страницу для детального просмотра статистики по курсу

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from reports.views import UsersMainStatisticsView, UsersListStatisticsView, UserDetailedStatisticsView, \
-    CoursesMainStatisticsView, CoursesListStatisticsView
+    CoursesMainStatisticsView, CoursesListStatisticsView, CoursesDetailedStatisticsView
 from reports.views.users_statistics_views.api import get_new_users_qty_view, get_test_progress_view
 from reports.views.courses_statistics_views.api import get_steps_completed_qty_view, get_lesson_types_view
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('user/<int:pk>/', UserDetailedStatisticsView.as_view(), name='stats_user_detailed'),
     path('courses/', CoursesMainStatisticsView.as_view(), name='stats_courses_main'),
     path('courses-list/', CoursesListStatisticsView.as_view(), name='stats_courses_list'),
+    path('courses/<int:pk>/', CoursesDetailedStatisticsView.as_view(), name='stats_course_detailed'),
 
     path('get-new-users-qty/', get_new_users_qty_view, name='get_new_users_qty'),
     path('get-steps-completed-qty/', get_steps_completed_qty_view, name='get_steps_completed_qty'),
