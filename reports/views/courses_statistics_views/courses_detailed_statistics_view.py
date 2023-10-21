daa19915631b7c34d5d05e2e1b7a379653883499
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-21 08:54:24 +0600

    #282 statistics course detailed Добавила карточки на страницу для детального просмотра статистики по курсу

--- reports/views/courses_statistics_views/courses_detailed_statistics_view.py
+++ reports/views/courses_statistics_views/courses_detailed_statistics_view.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.db.models import Count
 from django.views.generic import TemplateView
+
+from modules.models.user_course_progress import UserCourseProgress
+from step.models import StepModel
+from subscription.models.user_subscription import UsersSubscription
 
 
 class CoursesDetailedStatisticsView(PermissionRequiredMixin, TemplateView):
@@ -8,5 +13,17 @@ class CoursesDetailedStatisticsView(PermissionRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['subscriptions_qty'] = UsersSubscription.objects.filter(subscription__course__pk=kwargs.get('pk')
+                                                                        ).aggregate(Count('pk')).get('pk__count')
 
+        steps = StepModel.objects.filter(chapter__course__pk=kwargs.get('pk')).order_by('serial_number')
+        # first_step = steps.first()
+        last_step = steps.last()
+        completions_qty = UserCourseProgress.objects.filter(step=last_step).count()
+        context['completions_qty'] = completions_qty
+
+
+        # нужно доработать!!!
+        avg_completion_time = 0
+        context['avg_completion_time'] = avg_completion_time
         return context
