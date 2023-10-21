9fd1ed64a46c6064425b48ead978f61362f6c5b7
RustemGaliulin <asmoderous@gmail.com>
2023-10-22 00:28:39 +0600

    #267 исправление багов переключил БД

--- accessibleBIM/settings.py
+++ accessibleBIM/settings.py
@@ -124,16 +124,16 @@ WSGI_APPLICATION = 'accessibleBIM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": env.db(),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": env.db(),
+}
 
 
 # Password validation
