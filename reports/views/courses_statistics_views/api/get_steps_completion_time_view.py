1faecf543904a98b6fed50e238bd4d190e0d84ef
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-22 11:05:40 +0600

    #282 statistics course detailed Добавила график c результатами тестов. Доработала график среднего времени прохождения урока.

--- reports/views/courses_statistics_views/api/get_steps_completion_time_view.py
+++ reports/views/courses_statistics_views/api/get_steps_completion_time_view.py
@@ -15,25 +15,12 @@ def get_steps_completion_time_view(request, *args, **kwargs):
 
     course_pk = request.GET.get('course')
     results_from_db = UserCourseProgress.objects.filter(step__chapter__course__pk=course_pk,
-        status=CourseProgressStatusChoices.FINISHED).values(
-        'step__title', completion_time=(F('updated_at') - F('created_at')))
+        status=CourseProgressStatusChoices.FINISHED).values('step__title').annotate(
+        completion_time=(Avg(F('updated_at') - F('created_at'))))
 
-    completion_time = {}
-    counts = {}
     for record in results_from_db:
-        title = record.get('step__title')
-        if completion_time.get(title):
-            completion_time[title] += record['completion_time']
-            counts[title] += 1
-        else:
-            completion_time[title] = record['completion_time']
-            counts[title] = 1
-
-
-    for key, value in completion_time.items():
-        completion_time = value / counts[key]
-        result['labels'].append(key)
-        result['values'].append(calculate_minutes(completion_time))
+        result['labels'].append(record['step__title'])
+        result['values'].append(calculate_minutes(record['completion_time']))
 
     return JsonResponse(result)
 
