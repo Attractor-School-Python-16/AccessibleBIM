c5f40b1951c7b16ca683502c2a3667bc156cdb71
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-22 14:01:13 +0600

    #282 statistics Доработала апи для статистики по продажам

--- reports/views/sales_statistics_views/api/get_new_subscriptions_qty_view.py
+++ reports/views/sales_statistics_views/api/get_new_subscriptions_qty_view.py
@@ -9,25 +9,26 @@ from subscription.models.user_subscription import UsersSubscription
 
 @permission_required('accounts.can_view_sales_statistics')
 def get_new_subscriptions_qty_view(request, *args, **kwargs):
-    result = {
-        'error': False,
-        'error_messages': [],
-        'labels': [],
-        'values': []
-    }
+    if request.method == "GET":
+        result = {
+            'error': False,
+            'error_messages': [],
+            'labels': [],
+            'values': []
+        }
 
-    try:
-        days = int(request.GET.get('days'))
-    except ValueError:
-        result['error'] = True
-        result['error_messages'].append('Invalid query parameter')
+        try:
+            days = int(request.GET.get('days'))
+        except ValueError:
+            result['error'] = True
+            result['error_messages'].append('Invalid query parameter')
+            return JsonResponse(result)
+
+        first_day = date.today() - timedelta(days=days - 1)
+        results_from_db = UsersSubscription.objects.filter(create_at__date__gte=first_day).values(
+            'create_at__date').annotate(count=Count('pk')).order_by('create_at__date')
+        result['labels'] = [first_day + timedelta(days=day) for day in range(days)]
+        result['values'] = [0 for _ in range(days)]
+        for record in results_from_db:
+            result['values'][result['labels'].index(record['create_at__date'])] = record['count']
         return JsonResponse(result)
-
-    first_day = date.today() - timedelta(days=days - 1)
-    results_from_db = UsersSubscription.objects.filter(create_at__date__gte=first_day).values(
-        'create_at__date').annotate(count=Count('pk')).order_by('create_at__date')
-    result['labels'] = [first_day + timedelta(days=day) for day in range(days)]
-    result['values'] = [0 for _ in range(days)]
-    for record in results_from_db:
-        result['values'][result['labels'].index(record['create_at__date'])] = record['count']
-    return JsonResponse(result)
