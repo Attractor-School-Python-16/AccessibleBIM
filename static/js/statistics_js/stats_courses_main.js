d8b3b2a472e3dd321e19c330309def6be0b22b42
Anastassiya <anastassiya.belogurova@gmail.com>
2023-10-27 22:25:29 +0600

    #331 statistics design charts Изменила легенды графиков + рефакторинг js

--- static/js/statistics_js/stats_courses_main.js
+++ static/js/statistics_js/stats_courses_main.js
@@ -5,16 +5,21 @@ let stepsCompletedButtons = $('.steps-completed-chart-btn');
 
 let chartUrlsByDays = {
     'lessonTypesChart': {
+        title: 'Типы уроков',
         url: '/statistics/get-lesson-types/',
         div: lessonTypesChartDiv,
+        type: 'pie',
     },
     'stepsCompletedChart': {
+        title: 'Уроков изучено',
         url: '/statistics/get-steps-completed-qty/?days=',
         div: stepsCompletedChartDiv,
-        btn_cls: 'steps-completed-chart-btn'
+        type: 'area',
+        btn_cls: 'steps-completed-chart-btn',
+        xaxis: 'datetime',
     },
 };
 
-renderChart('pie', 'lessonTypesChart');
-renderChart('area', 'stepsCompletedChart', param='7');
+renderChart('lessonTypesChart');
+renderChart('stepsCompletedChart', param='7');
 stepsCompletedButtons.click(changeDaysOnChart);
