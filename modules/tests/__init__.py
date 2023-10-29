f368a34fc07436ce740944cac425f578b11e23fc
Emir <malcikclashofclans@gmail.com>
2023-10-29 23:20:55 +0600

    #102 TestModuleForm

---
+++ modules/tests/__init__.py
@@ -0,0 +1,2 @@
+from .model_tests import *
+from .form_tests import *
--- quiz_bim/tests/utils.py
+++ quiz_bim/tests/utils.py
@@ -1,6 +1,9 @@
 from functools import wraps
+from io import BytesIO
 
+from PIL import Image
 from django.contrib.auth import get_user_model
+from django.core.files import File
 from django.test import TestCase
 
 
@@ -44,3 +47,11 @@ def login_superuser(test_func):
             self.client.logout()
 
     return wrapper
+
+
+def get_image_file(name='test.png', ext='png', size=(50, 50), color=(256, 0, 0)):
+    file_obj = BytesIO()
+    image = Image.new("RGB", size=size, color=color)
+    image.save(file_obj, ext)
+    file_obj.seek(0)
+    return File(file_obj, name=name)
