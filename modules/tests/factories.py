53545975a56155974de7512c1e256642afe27f37
Emir <malcikclashofclans@gmail.com>
2023-10-29 20:19:26 +0600

    #106 CourseTargetFactory

--- modules/tests/factories.py
+++ modules/tests/factories.py
@@ -8,3 +8,11 @@ class ModuleFactory(factory.django.DjangoModelFactory):
     title = factory.sequence(lambda n: f"Module {n}")
     description = factory.Faker('sentence', nb_words=10)
     image = factory.django.ImageField(filename='image.jpg')
+
+
+class CourseTargetFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'modules.CourseTargetModel'
+
+    title = factory.sequence(lambda n: f"Target {n}")
+    description = factory.Faker('sentence', nb_words=10)
