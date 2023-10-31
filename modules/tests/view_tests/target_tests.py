d78886a77e8051c8a9f7a04f112e215d3b7d670f
Emir <malcikclashofclans@gmail.com>
2023-10-31 19:13:42 +0600

    #106 TestCourseTargetCreateView

--- modules/tests/view_tests/target_tests.py
+++ modules/tests/view_tests/target_tests.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from django.urls import reverse
 
 from modules.models import CourseTargetModel
-from modules.tests import CourseTargetFactory
+from modules.tests.factories import CourseTargetFactory
 from quiz_bim.tests.utils import CustomTestCase, login_superuser
 
 
@@ -58,3 +58,57 @@ class TestCourseTargetDetailView(CustomTestCase):
         self.client.force_login(self.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+
+class TestCourseTargetCreateView(CustomTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.correct_data = {
+            "title": "Course Target",
+            "description": "Description"
+        }
+        cls.url = reverse("modules:coursetargetmodel_create")
+        super().setUpTestData()
+
+    @login_superuser
+    def test_create_view(self):
+        previous_count = CourseTargetModel.objects.count()
+        response = self.client.post(self.url, self.correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(CourseTargetModel.objects.count() - previous_count, 1)
+        course_target = CourseTargetModel.objects.latest('create_at')
+        self.assertRedirects(response, reverse("modules:coursetargetmodel_detail", kwargs={"pk": course_target.pk}))
+        self.assertEqual(course_target.title, self.correct_data['title'])
+        self.assertEqual(course_target.description, self.correct_data['description'])
+
+    def test_anonymous(self):
+        response = self.client.post(self.url, self.correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    def test_no_permissions(self):
+        self.client.force_login(self.user)
+        response = self.client.post(self.url, self.correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    @login_superuser
+    def test_empty_title(self):
+        invalid_data = {
+            "title": "",
+            "description": "Description",
+        }
+        previous_count = CourseTargetModel.objects.count()
+        response = self.client.post(self.url, invalid_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertFormError(response, 'form', 'title', 'Это поле обязательно для заполнения.')
+        self.assertEqual(CourseTargetModel.objects.count() - previous_count, 0)
+
+    @login_superuser
+    def test_empty_description(self):
+        correct_data = {
+            "title": "Course Target",
+            "description": "",
+        }
+        previous_count = CourseTargetModel.objects.count()
+        response = self.client.post(self.url, correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(CourseTargetModel.objects.count() - previous_count, 1)
