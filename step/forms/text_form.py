61356fb37f01323aacec4aa4733d2045c389e5ce
RustemGaliulin <asmoderous@gmail.com>
2023-10-31 18:16:16 +0600

    #354 лейблы Добавил более понятные лейблы в формы step, video, text, file

--- step/forms/text_form.py
+++ step/forms/text_form.py
@@ -7,7 +7,11 @@ class TextForm(forms.ModelForm):
     class Meta:
         model = TextModel
         fields = ['text_title', 'text_description', 'content']
-
+        labels = {
+            'text_title': 'Введите наименование лекции',
+            'text_description': 'Укажите описание лекции',
+            'content': 'Содержание лекции',
+        }
         widgets = {
             'text_description': SummernoteWidget(),
             'content': SummernoteWidget(),
