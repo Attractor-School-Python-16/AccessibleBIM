c477d57a5bffa85545e98f4d914208ee9bc175b6
Emir <malcikclashofclans@gmail.com>
2023-11-03 17:21:28 +0600

    #105 TestTeacherModel

--- modules/tests/model_tests.py
+++ modules/tests/model_tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from modules.tests.factories import ChapterFactory, CourseTargetFactory, CourseFactory, ModuleFactory
+from modules.tests.factories import ChapterFactory, CourseTargetFactory, CourseFactory, ModuleFactory, TeacherFactory
 
 
 class TestModuleModel(TestCase):
@@ -63,3 +63,20 @@ class TestChapterModel(TestCase):
         self.assertEqual(chapter.description, "Description")
         self.assertEqual(chapter.course, course)
 
+
+class TestTeacherModel(TestCase):
+    def test_teacher_factory_no_exception(self):
+        try:
+            teacher = TeacherFactory.create()
+        except Exception:
+            self.fail("Exception was raised")
+
+    def test_teacher_factory(self):
+        teacher = TeacherFactory.create(first_name="First", last_name="Last", father_name="Father name",
+                                        job_title="Job", corp="Corp", experience="Experience", description="Description")
+        self.assertEqual(teacher.first_name, "First")
+        self.assertEqual(teacher.last_name, "Last")
+        self.assertEqual(teacher.father_name, "Father name")
+        self.assertEqual(teacher.job_title, "Job")
+        self.assertEqual(teacher.corp, "Corp")
+        self.assertEqual(teacher.description, "Description")
