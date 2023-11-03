2e5bc1a0315502104c6291caacfdc82524e6f41d
Emir <malcikclashofclans@gmail.com>
2023-11-03 18:09:26 +0600

    #105 TestTeacherCreateView

--- modules/tests/view_tests/teacher_tests.py
+++ modules/tests/view_tests/teacher_tests.py
@@ -57,3 +57,98 @@ class TestTeacherDetailView(CustomTestCase):
     def test_not_found(self):
         response = self.client.get(reverse("modules:teachermodel_detail", kwargs={"pk": 999}))
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+
+class TestTeacherCreateView(CustomTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.correct_data = {
+            "first_name": "First",
+            "last_name": "Last",
+            "father_name": "Father",
+            "job_title": "Job",
+            "corp": "Corp",
+            "experience": "Experience",
+            "description": "Description"
+        }
+        cls.url = reverse("modules:teachermodel_create")
+        super().setUpTestData()
+
+    @login_superuser
+    def test_create_view(self):
+        previous_count = TeacherModel.objects.count()
+        response = self.client.post(self.url, data=self.correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(TeacherModel.objects.count() - previous_count, 1)
+        teacher = TeacherModel.objects.latest('create_at')
+        self.assertRedirects(response, reverse("modules:teachermodel_detail", kwargs={"pk": teacher.pk}))
+        self.assertEqual(teacher.first_name, self.correct_data['first_name'])
+        self.assertEqual(teacher.last_name, self.correct_data['last_name'])
+        self.assertEqual(teacher.father_name, self.correct_data['father_name'])
+        self.assertEqual(teacher.job_title, self.correct_data['job_title'])
+        self.assertEqual(teacher.corp, self.correct_data['corp'])
+        self.assertEqual(teacher.experience, self.correct_data['experience'])
+        self.assertEqual(teacher.description, self.correct_data['description'])
+
+    def test_anonymous(self):
+        previous_count = TeacherModel.objects.count()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(TeacherModel.objects.count() - previous_count, 0)
+
+    @login_user
+    def test_no_permissions(self):
+        previous_count = TeacherModel.objects.count()
+        response = self.client.post(self.url, data=self.correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+        self.assertEqual(TeacherModel.objects.count() - previous_count, 0)
+
+    @login_superuser
+    def test_empty_first_name(self):
+        invalid_data = {
+            "first_name": "",
+            "last_name": "Last",
+            "father_name": "Father",
+            "job_title": "Job",
+            "corp": "Corp",
+            "experience": "Experience",
+            "description": "Description"
+        }
+        previous_count = TeacherModel.objects.count()
+        response = self.client.post(self.url, data=invalid_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertFormError(response, 'form', 'first_name', 'Это поле обязательно для заполнения.')
+        self.assertEqual(TeacherModel.objects.count() - previous_count, 0)
+
+    @login_superuser
+    def test_empty_last_name(self):
+        invalid_data = {
+            "first_name": "First",
+            "last_name": "",
+            "father_name": "Father",
+            "job_title": "Job",
+            "corp": "Corp",
+            "experience": "Experience",
+            "description": "Description"
+        }
+        previous_count = TeacherModel.objects.count()
+        response = self.client.post(self.url, data=invalid_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertFormError(response, 'form', 'last_name', 'Это поле обязательно для заполнения.')
+        self.assertEqual(TeacherModel.objects.count() - previous_count, 0)
+
+    @login_superuser
+    def test_null_fields(self):
+        correct_data = {
+            "first_name": "First",
+            "last_name": "Last",
+            "father_name": "",
+            "job_title": "",
+            "corp": "",
+            "experience": "",
+            "description": ""
+        }
+        previous_count = TeacherModel.objects.count()
+        response = self.client.post(self.url, data=correct_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(TeacherModel.objects.count() - previous_count, 1)
