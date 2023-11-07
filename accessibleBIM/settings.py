04aec3a02129dbf3f0e86a1d9dd03b88ad609e2d
RustemGaliulin <asmoderous@gmail.com>
2023-11-08 02:16:39 +0600

    #388 настройки обновлены настройки

--- accessibleBIM/settings.py
+++ accessibleBIM/settings.py
@@ -27,7 +27,7 @@ env = Env()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
