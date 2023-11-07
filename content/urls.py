8de8fecc8fb52bce45aaf0a5efc4cf897a6b329c
Anastassiya <anastassiya.belogurova@gmail.com>
2023-11-07 11:34:15 +0600

    #356 partners slider Добавила html код для админки, внесла небольшие изменеия во вью и эндпоинты

--- content/urls.py
+++ content/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-from content.views import ListPartnersView, PartnerDetailView, AddPartnerView, UpdatePartnerView, DeletePartnerView
+from content.views import ListPartnersView, AddPartnerView, UpdatePartnerView, DeletePartnerView
 
 app_name = 'content'
 
 urlpatterns = [
     path('partners/', ListPartnersView.as_view(), name='partners_list'),
-    path('partners/<int:pk>/', PartnerDetailView.as_view(), name='partner_detailed'),
     path('partners/add/', AddPartnerView.as_view(), name='add_partner'),
-    path('partners/update/', UpdatePartnerView.as_view(), name='update_partner'),
-    path('partners/delete/', DeletePartnerView.as_view(), name='delete_partner'),
+    path('partners/<int:pk>/update/', UpdatePartnerView.as_view(), name='update_partner'),
+    path('partners/<int:pk>/delete/', DeletePartnerView.as_view(), name='delete_partner'),
 ]
