5422f9e1ef726906417ce4afd190417607ba6592
Anastassiya <anastassiya.belogurova@gmail.com>
2023-11-13 23:38:15 +0600

    #163 multilanguage step Обновила модели в приложении step под мультиязычность

--- step/models/text.py
+++ step/models/text.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from modules.models import AbstractModel
 
 
 class TextModel(AbstractModel):
-    text_title = models.CharField(max_length=250, blank=True, null=True, verbose_name="Оглавление")
-    text_description = models.CharField(max_length=1500, blank=True, null=True, verbose_name="Описание")
-    content = models.TextField(max_length=100000, blank=True, null=True, verbose_name="Содержимое")
+    text_title = models.CharField(max_length=250, blank=True, null=True, verbose_name=_('Text title'))
+    text_description = models.CharField(max_length=1500, blank=True, null=True, verbose_name=_('Text description'))
+    content = models.TextField(max_length=100000, blank=True, null=True, verbose_name=_('Text content'))
 
     def get_absolute_url(self):
         return reverse("step:text_list")
+
     def __str__(self):
         return f'Текст {self.id} {self.text_title}'
 
     class Meta:
         db_table = 'texts'
-        verbose_name = 'Текст'
-        verbose_name_plural = 'Тексты'
+        verbose_name = _('Text')
+        verbose_name_plural = _('Texts')
