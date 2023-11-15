df3400314d91eb252ba7ebf3dbb34c90d0540686
arturateo <arti.shishka11@gmail.com>
2023-11-15 17:22:00 +0600

    #319 Тест 31 Deploy for Server with Docker

--- accessibleBIM/settings.py
+++ accessibleBIM/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = Env()
 
+#TEST1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
