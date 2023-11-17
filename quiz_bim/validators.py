e2f8a1a1719bf6fdc8a9153ee379942c44366580
Anastassiya <anastassiya.belogurova@gmail.com>
2023-11-17 22:41:59 +0600

    #160 multilanguage quiz Изменила формы и валидаторы под мультиязычность

--- quiz_bim/validators.py
+++ quiz_bim/validators.py
@@ -1,8 +1,11 @@
+from django.utils.translation import gettext_lazy as _
+
 from quiz_bim.models import AnswerBim
 
 
 def validate_answer(form, question, answer, is_correct, update=False):
-    return check_for_errors(form, question,  answer, is_correct, update)
+    return check_for_errors(form, question, answer, is_correct, update)
+
 
 def check_for_errors(form, question, answer, is_correct, update):
     error_messages = []
@@ -27,21 +30,20 @@ def check_correct_answers_count(form, question, is_correct, update):
     if answers_count >= 1:
         if correct_answers and is_correct:
             if not instance_is_correct:
-                error_message = ("В вопросе не может быть более двух правильных ответов")
+                error_message = _("Question cannot have more than 2 right answers")
             return error_message
     return None
 
 
-#проверка полей проводится здесь же, поскольку ValidationError и self.add_errors в форме не работают (форма не
+# проверка полей проводится здесь же, поскольку ValidationError и self.add_errors в форме не работают (форма не
 # отображается), вероятно потому что не передается pk вопроса
 
 def check_fields(answer, is_correct):
     errors = []
     if not answer:
-        errors.append("Поле ответа на может быть пустым")
+        errors.append(_("Answer field cannot be empty"))
     if len(answer) < 2:
-        errors.append("Ответ должен быть длиной не менее двух символов")
-    if is_correct != True and is_correct != False:
-        errors.append("Поле верности ответа может принимать только булевые значения")
+        errors.append(_("Answer should contain at least 2 symbols"))
+    if is_correct is not True and is_correct is not False:
+        errors.append(_("Answer correctness field can take only boolean values"))
     return errors
-
