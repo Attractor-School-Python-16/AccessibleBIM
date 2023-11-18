243b326e61e41accc58bca51e564b318debf7afc
Em1r <122436191+Em1rloneum@users.noreply.github.com>
2023-11-18 20:26:01 +0600

    #110 TestFileDetailView

--- step/tests/tests_file/model_tests.py
+++ step/tests/tests_file/model_tests.py
@@ -1,21 +1,21 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from step.tests.factories import FileModelFactory
+from step.tests.factories import FileFactory
 
 
 class TestFileModel(TestCase):
     def test_file_factory_no_exception(self):
         try:
-            file = FileModelFactory.create()
+            file = FileFactory.create()
         except Exception:
             self.fail("Exception was raised")
 
     def test_file_factory(self):
-        file = FileModelFactory.create(file_title="File")
+        file = FileFactory.create(file_title="File")
         self.assertEqual(file.file_title, "File")
 
     def test_file_functions(self):
-        file = FileModelFactory.create(file_title="File")
+        file = FileFactory.create(file_title="File")
         self.assertEqual(str(file), f'Файл {file.id} File')
         self.assertEqual(file.get_absolute_url(), reverse('step:filemodel_list'))
--- step/tests/tests_file/view_tests.py
+++ step/tests/tests_file/view_tests.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 from quiz_bim.tests.utils import CustomTestCase, login_superuser, login_user
 from step.models import FileModel
+from step.tests.factories import FileFactory
 
 
 class TestFileListView(CustomTestCase):
@@ -20,6 +21,34 @@ class TestFileListView(CustomTestCase):
         self.assertTemplateUsed(response, 'steps/file/file_list.html')
         self.assertFalse(response.context['is_paginated'])
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
+
+    def test_anonymous(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    @login_user
+    def test_no_permissions(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+
+class TestFileDetailView(CustomTestCase):
+    def setUp(self):
+        self.file = FileFactory.create()
+        self.url = reverse("step:filemodel_detail", kwargs={"pk": self.file.pk})
+
+    @login_superuser
+    def test_file_detail_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context['file'], self.file)
+        self.assertTemplateUsed(response, 'steps/file/file_detail.html')
+        self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
+
+    @login_superuser
+    def test_not_found(self):
+        response = self.client.get(reverse("step:filemodel_detail", kwargs={"pk": 999}))
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_anonymous(self):
         response = self.client.get(self.url)
